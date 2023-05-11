d331e94e8ea74c711115649293971a35120fdebe
CLI Arena <cateru.dev@zohomail.com>
2023-05-11 12:47:49 +0100

    feat(x): added catppucin theme to nixvim

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }: {
   programs.nixvim = {
     enable = true;
+    extraPlugins = with pkgs.vimPlugins;
+      [
+        catppuccin-nvim # # Theme
+      ];
+    colorscheme = "catppuccin";
   };
 }
