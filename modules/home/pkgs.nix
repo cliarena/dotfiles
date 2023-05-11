a190a109397c3239d16bf0e798a344fb4de83dea
CLI Arena <cateru.dev@zohomail.com>
2023-05-11 16:25:35 +0100

    feat(x): nixvim: moved home pkgs to system packages

--- modules/home/pkgs.nix
+++ modules/home/pkgs.nix
@@ -6,15 +6,4 @@
       nix-direnv.enable = true;
     };
   };
-  home.packages = with pkgs; [
-    manix # # CLI Searcher for Nix, HM documentation
-    jq # # CLI JSON processor
-    jqp # # jq playground
-    htmlq # # jq fo html
-    fd # # Search file names
-    ripgrep # # Fastest Sercher
-    ripgrep-all # # Better rg
-    wget
-    ventoy-bin # multiple Bootable usb drives in one
-  ];
 }
--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,26 +1,38 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
+    ### Git ###
+    gh
     git
     commitizen
-    bat
-    jq
-    unzip
-    speedtest-cli
-    httpie
-    nmap
-    musikcube
 
+    ### Web ###
+    w3m
+    python310Packages.howdoi
     # mpv
     # ytfzf
 
-    ### Search ###
-    gh
-    w3m
-    python310Packages.howdoi
-
-    # programing
+    ### Programing ###
+    wget
+    jq # # CLI JSON processor
+    jqp # # jq playground
+    htmlq # # jq fo html
+    httpie
     pgcli
+    manix # # CLI Searcher for Nix, HM documentation
 
+    ### Search ###
+    fd # # Search file names
+    fzy # # fzf but better
+    ripgrep # # Fastest Sercher
+    ripgrep-all # # Better rg
+
+    ### Tools ###
+    bat
+    unzip
+    speedtest-cli
+    nmap
+    musikcube
+    ventoy-bin # multiple Bootable usb drives in one
   ];
 
 }
