ea5e2161220a3eda9c248a4683d916cf57970383
CLI Arena <cateru.dev@zohomail.com>
2023-05-17 14:40:32 +0100

    feat(x): fonts: added Hacked-kerx font

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }: {
 
   # fonts that are reachable by applications. ie: figma
-  fonts.fonts = [
-    # builtins.readFile(./custom_font.ttf) # custom font
-    pkgs.google-fonts
-    pkgs.nerdfonts
-  ];
+  fonts.fonts = [ ./techy pkgs.google-fonts pkgs.nerdfonts ];
 
 }
