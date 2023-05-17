4661c1032c5e8b5a70f4698fc013b3e9ce098525
CLI Arena <cateru.dev@zohomail.com>
2023-05-17 07:18:25 +0100

    fix(x): sops: %r path extension problem

--- hosts/x/sops.nix
+++ hosts/x/sops.nix
@@ -5,10 +5,12 @@ in {
     defaultSopsFile = ../../secrets/${user}.yaml;
     age.keyFile = ../../secrets/age-key.txt;
 
+    # must set when used with home-manager 
+    defaultSymlinkPath = "${homeDirectory}/.sops/secrets";
+    defaultSecretsMountPoint = "${homeDirectory}/.sops/secrets.d";
     secrets = {
       AGE_SECRET_KEY = {
         sopsFile = ../../secrets/default.yaml;
-        # path = "/home/x/.config/sops/age/keys.txt";
         path = "${homeDirectory}/.config/sops/age/keys.txt";
       };
       PI_SSH_KEY = { sopsFile = ../../secrets/ssh.yaml; };
