38647435ba6b18240f0641ace687097ad9875deb
CLI Arena <cateru.dev@zohomail.com>
2023-05-22 09:03:42 +0100

    fix(x): tty resolution

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,6 +2,7 @@
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
+    kernelParams = [ "video=1920x1080" ];
     initrd.availableKernelModules = [ "xhci_pci" "usbhid" "uas" "usb_storage" ];
     kernel.sysctl = { "vm.swappiness" = 10; };
 
