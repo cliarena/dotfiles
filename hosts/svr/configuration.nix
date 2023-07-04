1aaf535eff1086a8ce587029943b21dc305f160e
CLI Arena <git@cliarena.com>
2023-07-04 10:54:07 +0100

    fix(svr): removed unused modules kmonad and some fonts

--- hosts/svr/configuration.nix
+++ hosts/svr/configuration.nix
@@ -1,18 +1,13 @@
 { ... }: {
   imports = [
+    ./sops.nix
     ../../modules/nix_config.nix
     (import ../../modules/disko.nix { }) # doesn't support btrfs swapfile
     ../../modules/swap.nix
     ../../modules/boot.nix
     ../../modules/users.nix
-    ../../modules/fonts
-    # ../../modules/display_manager.nix
-    # ../../modules/hyprland.nix
     ../../modules/i18n.nix
-    # ../../modules/pipewire.nix
     ../../modules/network.nix
-    ../../modules/kmonad
-    # ../../modules/pkgs.nix
     ../../modules/auditd.nix
   ];
   system.stateVersion = "22.11";
