7ea72e1dd87be52980c899998a3bacfbb2ec0785
CLI Arena <git@cliarena.com>
2023-07-08 21:56:28 +0100

    feat(x): added vault keys to sops file

--- hosts/x/sops.nix
+++ hosts/x/sops.nix
@@ -16,6 +16,13 @@ in {
       PI_SSH_KEY = { sopsFile = ../../secrets/ssh.yaml; };
       GL_SSH_KEY = { sopsFile = ../../secrets/ssh.yaml; };
       SVR_SSH_KEY = { sopsFile = ../../secrets/ssh.yaml; };
+
+      VAULT_ROOT_TOKEN = { sopsFile = ../../secrets/vault.yaml; };
+      VAULT_UNSEAL_KEY_1 = { sopsFile = ../../secrets/vault.yaml; };
+      VAULT_UNSEAL_KEY_2 = { sopsFile = ../../secrets/vault.yaml; };
+      VAULT_UNSEAL_KEY_3 = { sopsFile = ../../secrets/vault.yaml; };
+      VAULT_UNSEAL_KEY_4 = { sopsFile = ../../secrets/vault.yaml; };
+      VAULT_UNSEAL_KEY_5 = { sopsFile = ../../secrets/vault.yaml; };
     };
   };
 
