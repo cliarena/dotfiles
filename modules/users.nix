21cb7e5a1cec41802845862edfb1206a02c026b9
CLI Arena <git@cliarena.com>
2023-07-13 18:54:08 +0100

    feat(shell): changed default shell to nu shell

--- modules/users.nix
+++ modules/users.nix
@@ -3,7 +3,7 @@
     isNormalUser = true;
     initialPassword = "nixos";
     extraGroups = [ "wheel" ];
-    # shell = pkgs.nushell;
+    shell = pkgs.nushell;
     openssh.authorizedKeys.keys = host.ssh_authorized_keys;
   };
   # Use sudo with no password
