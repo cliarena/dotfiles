d173a9eacf425498bef686134dd2a0abd52995ff
CLI Arena <git@cliarena.com>
2023-07-17 13:13:54 +0100

    feat(boot): added iptables to kernel modules

--- modules/boot.nix
+++ modules/boot.nix
@@ -4,6 +4,15 @@
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "video=1920x1080" ];
     initrd.availableKernelModules = [ "xhci_pci" "usbhid" "uas" "usb_storage" ];
+    kernelModules = [
+      # need for nomad iptables
+      "iptable_nat"
+      "iptable_filter"
+      "xt_nat"
+      "xt_mark"
+      "xt_comment"
+      "xt_multiport"
+    ];
     kernel.sysctl = { "vm.swappiness" = 10; };
 
     loader = {
