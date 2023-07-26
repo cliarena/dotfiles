6a1e7183633039ef1f264ef8fb5b0746052f9a30
CLI Arena <git@cliarena.com>
2023-07-26 02:22:53 +0100

    fix(hashi_stack): added envoy to consul api gateway registerer

--- modules/hashi_stack/consul/api_gateway_regiterer.nix
+++ modules/hashi_stack/consul/api_gateway_regiterer.nix
@@ -3,7 +3,7 @@ let consul_bin = "${pkgs.consul}/bin/consul";
 in {
 
   systemd.services.consul_api_gateway_registerer = {
-    path = [ pkgs.getent ];
+    path = [ pkgs.getent pkgs.envoy ];
     description = "register consul api gateways";
     script = ''
       ${consul_bin} connect envoy -gateway api -register -service cliarena_gateway
