354f38eb5188272124536fb31c9af91c5fac53d5
CLI Arena <git@cliarena.com>
2023-09-06 17:58:43 +0100

    docs(hardware): added doc

--- modules/hardware/amd.nix
+++ modules/hardware/amd.nix
@@ -1,4 +1,4 @@
 { ... }: {
-  hardware.cpu.amd.updateMicrocode = true;
+  hardware.cpu.amd.updateMicrocode = true; # Maybe it fixed TTY scale issue
   hardware.enableRedistributableFirmware = true; # to detect wireless interfaces
 }
