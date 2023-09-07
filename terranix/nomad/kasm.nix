8aa0de01fc5d214de15d64bdeb7f541aa8401326
CLI Arena <git@cliarena.com>
2023-09-07 20:42:30 +0100

    fix(terranix): updated kasm job

--- terranix/nomad/kasm.nix
+++ terranix/nomad/kasm.nix
@@ -1,14 +1,15 @@
 { time, ... }:
 let
   http = {
-    reservedPorts.wizard = {
-      static = 3000;
-      to = 3000;
-    };
-    reservedPorts.http = {
-      static = 443;
-      to = 443;
-    };
+    mode = "bridge";
+    #reservedPorts.wizard = {
+    #  static = 3000;
+    #  to = 3000;
+    #};
+    #reservedPorts.http = {
+    #  static = 8443;
+    #  to = 443;
+    #};
   };
 in {
   job.kasm = {
@@ -33,8 +34,18 @@ in {
       networks = [ http ];
       #      mode = "bridge"
 
+      services = [{
+        name = "kasm";
+        # WARN: Don't use named ports ie: port ="http". use literal ones
+        port = "443";
+        connect = {
+          sidecarService = {
+            # port = "20000";
+          };
+        };
+      }];
       task.server = {
-        driver = "podman";
+        driver = "docker";
 
         env = { KASM_PORT = "443"; };
 
@@ -52,7 +63,7 @@ in {
         config = {
           image = "lscr.io/linuxserver/kasm:latest";
           privileged = true;
-          ports = [ "http" ];
+          # ports = [ "http" ];
           #	volumes = [  "/vault/hdd/nomad/static-site:/usr/share/nginx/html" ]
         };
 
@@ -61,19 +72,19 @@ in {
           memory = 4000;
         };
 
-        services = [{
-          name = "kasm";
-          port = "http";
+        #services = [{
+        #   name = "kasm";
+        # port = "http";
 
-          tags = [ "kasm" ];
+        #  tags = [ "kasm" ];
 
-          checks = with time; [{
-            type = "http";
-            path = "/api/__healthcheck";
-            interval = 2 * second;
-            timeout = 2 * second;
-          }];
-        }];
+        #  checks = with time; [{
+        #    type = "http";
+        #    path = "/api/__healthcheck";
+        #    interval = 2 * second;
+        #    timeout = 2 * second;
+        #  }];
+        # }];
       };
     };
   };
