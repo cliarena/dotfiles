40ac147f462c04fc778780d7b669442f1099d007
CLI Arena <git@cliarena.com>
2023-09-19 11:06:40 +0100

    fix(hashi_stack): changed consul proxy defaults protocol to http

--- modules/hashi_stack/consul/main.nix
+++ modules/hashi_stack/consul/main.nix
@@ -83,7 +83,7 @@
           {
             kind = "proxy-defaults";
             name = "global";
-            config = { protocol = "tcp"; };
+            config = { protocol = "http"; };
           }
           {
             name = "cliarena-gateway";
