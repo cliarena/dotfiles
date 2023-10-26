fb639822856fe58f023c68dcb66c8829272e7348
CLI Arena <git@cliarena.com>
2023-10-26 17:24:07 +0100

    fix(hashi_stack): disabled api gateway registerer

--- modules/hashi_stack/consul/default.nix
+++ modules/hashi_stack/consul/default.nix
@@ -1,4 +1,6 @@
 { ... }: {
   imports =
-    [ ./main.nix ./http_route_refresher.nix ./api_gateway_regiterer.nix ];
+    [ ./main.nix ./http_route_refresher.nix 
+    /* ./api_gateway_regiterer.nix -- TODO: look if its possible to update gateway config natively  to reduce dependency management to pin envoy compatibility with consul*/ 
+    ];
 }
