1c7160de1f6dccbaa45b0495028d6e619b33193b
CLI Arena <git@cliarena.com>
2023-10-26 17:46:56 +0100

    Revert "fix(hashi_stack): pinned envoy version to one compatible with consul"
    This reverts commit cdbbf9403e036d7a62b7788ae9418548cca266a8.

--- modules/hashi_stack/envoy.nix
+++ modules/hashi_stack/envoy.nix
@@ -1,16 +1,6 @@
-{ ... }:
-let
-     pkgs = import (builtins.fetchGit {
-         # Descriptive name to make the store path easier to identify
-         name = "envoy-1.26.4";
-         url = "https://github.com/NixOS/nixpkgs/";
-         ref = "refs/heads/nixpkgs-unstable";
-         rev = "9957cd48326fe8dbd52fdc50dd2502307f188b0d";
-     }) {};
-in
-{
+{ ... }: {
   services.envoy = {
-    package = pkgs.envoy;
     enable = true;
+
   };
 }
