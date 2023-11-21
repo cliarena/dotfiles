d2dd574cead60d5060c583ee3bac8109a899ea62
CLI Arena <git@cliarena.com>
2023-11-21 09:58:12 +0100

    fix(containers): test using network config for dev-space

--- containers/dev_space.nix
+++ containers/dev_space.nix
@@ -1,13 +1,32 @@
 { inputs, nixpkgs, ... }:
 let
 
+  host = rec {
+    user = "x";
+    hostAddress = "10.10.2.1";
+    localAddress = "10.10.2.100";
+    wan_ips = [ "${localAddress}/24" ];
+    wan_gateway = [ hostAddress ];
+    is_dns_server = false; # for testing hashi_stack
+    dns_server = wan_gateway;
+    dns_extra_hosts = "";
+    ssh_authorized_keys = [ ];
+    ports = {
+      dns = 53;
+      ssh = 22;
+    };
+    # open the least amount possible
+    tcp_ports = with ports; [ dns ssh 8080 ];
+    udp_ports = with ports; [ dns ];
+  };
 in {
 
   containers.dev-space = {
+    inherit (host) hostAddress localAddress;
     autoStart = true;
     privateNetwork = true;
-    hostAddress = "10.10.2.1";
-    localAddress = "10.10.2.100";
+    # hostAddress = "10.10.2.1";
+    # localAddress = "10.10.2.100";
     ephemeral = true;
     config = { config, pkgs, ... }:
       let inherit (inputs) home-manager sops-nix;
@@ -21,22 +40,7 @@ in {
         '';
         system.stateVersion = "22.11";
 
-        _module.args.host = rec {
-          user = "x";
-          wan_ips = [ "10.10.0.100/24" ];
-          wan_gateway = [ "10.10.0.10" ];
-          is_dns_server = false; # for testing hashi_stack
-          dns_server = wan_gateway;
-          dns_extra_hosts = "";
-          ssh_authorized_keys = [ ];
-          ports = {
-            dns = 53;
-            ssh = 22;
-          };
-          # open the least amount possible
-          tcp_ports = with ports; [ dns ssh 8080 ];
-          udp_ports = with ports; [ dns ];
-        };
+        _module.args.host = host;
 
         imports = [
           inputs.nixvim.nixosModules.nixvim
@@ -47,6 +51,7 @@ in {
           ../modules/hardware/amd.nix
           ../modules/corectrl.nix
           ../modules/users.nix
+          ../modules/netwoking/network.nix
 
           home-manager.nixosModules.home-manager
           {
@@ -87,8 +92,8 @@ in {
           openFirewall = true;
           defaultWindowManager = "i3";
         };
-        networking.useHostResolvConf = pkgs.lib.mkForce false;
-        services.resolved.enable = true;
+        # networking.useHostResolvConf = pkgs.lib.mkForce false;
+        # services.resolved.enable = true;
 
       };
   };
