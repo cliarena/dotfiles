9cf242a17b58b9b1e519d3ef9ab65356a6335b40
CLI Arena <git@cliarena.com>
2023-12-01 22:11:34 +0100

    fix(containers): added dummy screen to dev-space

--- containers/dev_space.nix
+++ containers/dev_space.nix
@@ -204,32 +204,31 @@ in {
           displayManager.defaultSession = "gnome";
 
           # Dummy screen
-          /* monitorSection = ''
-               VendorName     "Unknown"
-               HorizSync   30-85
-               VertRefresh 48-120
+          monitorSection = ''
+            VendorName     "Unknown"
+            HorizSync   30-85
+            VertRefresh 48-120
 
-               ModelName      "Unknown"
-               Option         "DPMS"
-             '';
+            ModelName      "Unknown"
+            Option         "DPMS"
+          '';
 
-             deviceSection = ''
-               VendorName "NVIDIA Corporation"
-               Option      "AllowEmptyInitialConfiguration"
-               Option      "ConnectedMonitor" "DFP"
-               Option      "CustomEDID" "DFP-0"
+          deviceSection = ''
+            VendorName "NVIDIA Corporation"
+            Option      "AllowEmptyInitialConfiguration"
+            Option      "ConnectedMonitor" "DFP"
+            Option      "CustomEDID" "DFP-0"
 
-             '';
+          '';
 
-             screenSection = ''
-               DefaultDepth    24
-               Option      "ModeValidation" "AllowNonEdidModes, NoVesaModes"
-               Option      "MetaModes" "1920x1080"
-               SubSection     "Display"
-                   Depth       24
-               EndSubSection
-             '';
-          */
+          screenSection = ''
+            DefaultDepth    24
+            Option      "ModeValidation" "AllowNonEdidModes, NoVesaModes"
+            Option      "MetaModes" "1920x1080"
+            SubSection     "Display"
+                Depth       24
+            EndSubSection
+          '';
         };
 
         # Sunshine user, service and config 
