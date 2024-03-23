2e91c063a8072b352cf700297684d7159c21fa5a
CLI Arena <git@cliarena.com>
2024-03-23 15:36:58 +0000

    feat(nushell): added completion & ux emprovements

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,34 +1,97 @@
-{ ... }: {
-  programs.nushell = {
-    enable = true;
-    configFile.text = ''
-      $env.config = {
-        show_banner: false,
-        edit_mode: vi,
-      }
-    '';
-    envFile.text = ''
-      def create_left_prompt [] {
-      starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
-      }
-      load-env {
-      STARSHIP_SHELL : "nu",
-      # Use nushell functions to define your right and left prompt
-      PROMPT_COMMAND : { || create_left_prompt },
-      PROMPT_COMMAND_RIGHT : "",
-      # The prompt indicators are environmental variables that represent
-      # the state of the prompt
-      PROMPT_INDICATOR : "",
-      PROMPT_INDICATOR_VI_INSERT : ": ",
-      PROMPT_INDICATOR_VI_NORMAL : "  ",
-      PROMPT_MULTILINE_INDICATOR : "::: ",
-      }
-    '';
-    shellAliases = {
-      l = "ls";
-      ll = "ls -l";
-      la = "ls -a";
+{ pkgs, ... }: {
+  programs = {
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
     };
+    atuin = {
+      enable = true;
+      enableNushellIntegration = true;
+      settings = { };
+    };
+    yazi = {
+      enable = true;
+      enableNushellIntegration = true;
+      settings = {
+        log = { enabled = false; };
+        manager = {
+          show_hidden = false;
+          # sort_by = "modified";
+          sort_dir_first = true;
+          sort_reverse = true;
+        };
+      };
+      keymap = {
+        input.keymap = [
+          {
+            exec = "close";
+            on = [ "" ];
+          }
+          {
+            exec = "close --submit";
+            on = [ "" ];
+          }
+          {
+            exec = "escape";
+            on = [ "" ];
+          }
+          {
+            exec = "backspace";
+            on = [ "" ];
+          }
+        ];
+        manager.keymap = [
+          {
+            exec = "escape";
+            on = [ "" ];
+          }
+          {
+            exec = "quit";
+            on = [ "q" ];
+          }
+          {
+            exec = "close";
+            on = [ "" ];
+          }
+        ];
+      };
+    };
+    zoxide = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
+    nushell = {
+      enable = true;
+      configFile.text = ''
+        $env.config = {
+          show_banner: false,
+          edit_mode: vi,
+        }
+      '';
+      envFile.text = ''
+        def create_left_prompt [] {
+        starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
+        }
+        load-env {
+        STARSHIP_SHELL : "nu",
+        # Use nushell functions to define your right and left prompt
+        PROMPT_COMMAND : { || create_left_prompt },
+        PROMPT_COMMAND_RIGHT : "",
+        # The prompt indicators are environmental variables that represent
+        # the state of the prompt
+        PROMPT_INDICATOR : "",
+        PROMPT_INDICATOR_VI_INSERT : ": ",
+        PROMPT_INDICATOR_VI_NORMAL : "  ",
+        PROMPT_MULTILINE_INDICATOR : "::: ",
+        }
+      '';
+      shellAliases = {
+        l = "ls";
+        ll = "ls -l";
+        la = "ls -a";
+      };
+    };
+    programs.starship.enable = true;
   };
-  programs.starship.enable = true;
 }
