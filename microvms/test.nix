2d39bb9090608e2d599b02d6d1849953189f2e96
CLI Arena <git@cliarena.com>
2024-03-23 07:31:16 +0000

    feat(microvms): disabled test vm from autostarting

--- microvms/test.nix
+++ microvms/test.nix
@@ -3,6 +3,7 @@
     my-microvm = {
       # The package set to use for the microvm. This also determines the microvm's architecture.
       # Defaults to the host system's package set if not given.
+      autostart = false;
       inherit pkgs;
 
       # (Optional) A set of special arguments to be passed to the MicroVM's NixOS modules.
