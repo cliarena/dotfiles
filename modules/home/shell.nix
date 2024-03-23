ced21b094179fdfd5fb05ea0d6d9147a2d94cecd
CLI Arena <git@cliarena.com>
2024-03-23 17:43:30 +0000

    fix(nushell): fixed yazi config

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -21,40 +21,40 @@
           sort_reverse = true;
         };
       };
-      keymap = {
-        input.keymap = [
-          {
-            exec = "close";
-            on = [ "" ];
-          }
-          {
-            exec = "close --submit";
-            on = [ "" ];
-          }
-          {
-            exec = "escape";
-            on = [ "" ];
-          }
-          {
-            exec = "backspace";
-            on = [ "" ];
-          }
-        ];
-        manager.keymap = [
-          {
-            exec = "escape";
-            on = [ "" ];
-          }
-          {
-            exec = "quit";
-            on = [ "q" ];
-          }
-          {
-            exec = "close";
-            on = [ "" ];
-          }
-        ];
-      };
+      # keymap = {
+      # input.keymap = [
+      # {
+      # exec = "close";
+      # on = [ "" ];
+      # }
+      # {
+      # exec = "close --submit";
+      # on = [ "" ];
+      # }
+      # {
+      # exec = "escape";
+      # on = [ "" ];
+      # }
+      # {
+      # exec = "backspace";
+      # on = [ "" ];
+      # }
+      # ];
+      # manager.keymap = [
+      # {
+      # exec = "escape";
+      # on = [ "" ];
+      # }
+      # {
+      # exec = "quit";
+      # on = [ "q" ];
+      # }
+      # {
+      # exec = "close";
+      # on = [ "" ];
+      # }
+      # ];
+      # };
     };
     zoxide = {
       enable = true;
