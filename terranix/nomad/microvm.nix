b90e496a3313a4cdac58683640c80bebd9c3e35f
CLI Arena <git@cliarena.com>
2024-03-26 09:54:39 +0000

    fix(terranix_nomad): testing other microvm config

--- terranix/nomad/microvm.nix
+++ terranix/nomad/microvm.nix
@@ -1,8 +1,9 @@
-{ time, pkgs, self, config, microvm, ... }:
+{ pkgs, self, config, microvm, ... }:
 
 let
   inherit (pkgs) lib;
-  inherit (time) second;
+  # inherit (time) second;
+  second = 1000000000;
   user = "svr";
   repo = "test";
   vmName = "my-microvm";
@@ -12,6 +13,13 @@ let
     self.nixosConfigurations.svr.config.microvm.vms.my-microvm.config.config.microvm;
   runner = microvm.declaredRunner;
   inherit (lib.last microvm.shares) tag source socket;
+  http = {
+    mode = "bridge";
+    reservedPorts.http = {
+      static = 9999;
+      to = 8080;
+    };
+  };
 in {
 
   job.microvm = {
@@ -30,34 +38,51 @@ in {
         unlimited = true;
         delay = 90 * second;
       };
-      task."virtiofsd-${tag}" = {
-        lifecycle = {
-          hook = "prestart";
-          sidecar = true;
-        };
-        driver = "raw_exec";
-        user = "root";
-        config = { command = "local/virtiofsd-${tag}.sh"; };
-        templates = [{
-          destination = "local/virtiofsd-${tag}.sh";
-          perms = "755";
-          data = ''
-            #! /run/current-system/sw/bin/bash -e
-            mkdir -p ${workDir}
-            chown microvm:kvm ${workDir}
-            cd ${workDir}
-            mkdir -p ${source}
-            exec /run/current-system/sw/bin/virtiofsd \
-            --socket-path=${socket} \
-            --socket-group=kvm \
-            --shared-dir=${source} \
-            --sandbox=none \
-            --thread-pool-size `nproc` \
-            --cache=always
-          '';
-        }];
-        killTimeout = 5 * second;
-      };
+      networks = [ http ];
+      services = [{
+        name = "microvm";
+        # WARN: Don't use named ports ie: port ="http". use literal ones
+        port = "8080";
+        connect = { sidecarService = { }; };
+        # task = "hypervisor";
+        # checks = [{
+        # type = "http";
+        # path = "/";
+        # # protocol = "https";
+        # # expose = true;
+        # # tlsSkipVerify = true;
+        # interval = 3 * second;
+        # timeout = 2 * second;
+        # }];
+      }];
+      # task."virtiofsd-${tag}" = {
+      # lifecycle = {
+      # hook = "prestart";
+      # sidecar = true;
+      # };
+      # driver = "raw_exec";
+      # user = "root";
+      # config = { command = "local/virtiofsd-${tag}.sh"; };
+      # templates = [{
+      # destination = "local/virtiofsd-${tag}.sh";
+      # perms = "755";
+      # data = ''
+      # #! /run/current-system/sw/bin/bash -e
+      # mkdir -p ${workDir}
+      # chown microvm:kvm ${workDir}
+      # cd ${workDir}
+      # mkdir -p ${source}
+      # exec /run/current-system/sw/bin/virtiofsd \
+      # --socket-path=${socket} \
+      # --socket-group=kvm \
+      # --shared-dir=${source} \
+      # --sandbox=none \
+      # --thread-pool-size `nproc` \
+      # --cache=always
+      # '';
+      # }];
+      # killTimeout = 5 * second;
+      # };
 
       task.copy_system = {
         driver = "raw_exec";
@@ -76,9 +101,90 @@ in {
         }];
       };
 
+      task.volume-dirs = {
+        driver = "raw_exec";
+        lifecycle = { hook = "prestart"; };
+        config = { command = "local/make-dirs.sh"; };
+        templates = [{
+          destination = "local/make-dirs.sh";
+          perms = "755";
+          data = ''
+            #! /run/current-system/sw/bin/bash -e
+            ${lib.concatMapStrings ({ image, ... }: ''
+              mkdir -p "${dirOf image}"
+              chown microvm:kvm "${dirOf image}"
+            '') microvm.volumes}
+          '';
+        }];
+      };
+
+      # ${
+      # lib.concatMapStrings (id:
+      # with config.skyflake.deploy.rbds.${id}; ''
+      # task."rbd-map-${id}" = {
+      # driver = "raw_exec";
+      # lifecycle ={
+      # hook = "prestart";
+      # };
+      # config= {
+      # command = "local/rbd-map.sh";
+      # };
+      # templates = [{
+      # destination = "local/rbd-map.sh";
+      # perms = "755";
+      # data = ''
+      # #! /run/current-system/sw/bin/bash -e
+      # PATH="$PATH:/run/current-system/sw/bin"
+      # SPEC="${pool}/${namespace}/${name}"
+      # ${lib.optionalString autoCreate ''
+      # if ! rbd info "$SPEC" >/dev/null 2>/dev/null; then
+      # rbd namespace create "${pool}/${namespace}" || true
+      # rbd create --size ${toString size}M "$SPEC"
+      # TARGET=$(rbd map "$SPEC")
+      # mkfs.${fsType} -L ${
+      # builtins.substring 0 16 "${user}-${vmName}"
+      # } "$TARGET"
+      # rbd unmap "$TARGET"
+      # fi
+      # ''}
+      # OLD_MAPS=$(rbd showmapped --format json | jq -r '.[] | select(.pool == "${pool}" and .namespace == "${namespace}" and .name == "${name}") | .device')
+      # for OLD_TARGET in $OLD_MAPS; do
+      # rbd unmap "$OLD_TARGET" || true
+      # done
+      # TARGET=$(rbd map --exclusive "$SPEC")
+      # echo "Mapped $SPEC to $TARGET">&2
+      # chown microvm "$TARGET"
+      # cd ${workDir}
+      # mkdir -p $(dirname "${path}")
+      # ln -sf "$TARGET" "${path}"''
+      # };
+      # };
+      # task "rbd-unmap-${id}" {
+      # driver = "raw_exec"
+      # lifecycle {
+      # hook = "poststop"
+      # }
+      # config {
+      # command = "local/rbd-unmap.sh"
+      # }
+      # template {
+      # destination = "local/rbd-unmap.sh"
+      # perms = "755"
+      # data = <<EOD
+      # #! /run/current-system/sw/bin/bash -e
+      # PATH="$PATH:/run/current-system/sw/bin"
+      # cd ${workDir}
+      # rbd unmap $(readlink "${path}")
+      # EOD
+      # }
+      # }
+      # '') (builtins.attrNames config.skyflake.deploy.rbds)
+      # }
+
       task.hypervisor = {
         driver = "raw_exec";
-        user = "microvm";
+        # user = "microvm";
+        user = "root";
         config = { command = "local/hypervisor.sh"; };
         templates = [{
           destination = "local/hypervisor.sh";
@@ -119,25 +225,4 @@ in {
       };
     };
   };
-
 }
-# in pkgs.stdenv.mkDerivation rec {
-# pname = "${user}-${repo}-${vmName}";
-# inherit (config.system.nixos) version;
-# src = jobFile;
-# NAME = "${pname}.job";
-# phases = [ "buildPhase" "checkPhase" "installPhase" ];
-# buildInputs = lib.optionals (pkgs ? hclfmt) [ pkgs.hclfmt ];
-# buildPhase = if pkgs ? hclfmt then ''
-# hclfmt < $src > $NAME
-# '' else ''
-# cp $src $NAME
-# '';
-# checkInputs = with pkgs; [ nomad ];
-# checkPhase = ''
-# nomad job validate $NAME
-# '';
-# installPhase = ''
-# cp $NAME $out
-# '';
-# }
