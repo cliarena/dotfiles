327cbe107d12729c982d02cc8664a19d5db7ea8a
CLI Arena <git@cliarena.com>
2024-04-20 12:31:44 +0100

    feat(git): added commit signing for gitlab

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -3,6 +3,12 @@
     enable = true;
     userName = "CLI Arena";
     userEmail = "git@cliarena.com";
-    extraConfig = { init.defaultBranch = "main"; };
+    signing.signByDefault = true;
+    signing.key =
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDppFak0qD2HXu9mtYXfrjzKFqPFtKzzmnBP5dNQr4Me GitLab";
+    extraConfig = {
+      init.defaultBranch = "main";
+      gpg.format = "ssh";
+    };
   };
 }
