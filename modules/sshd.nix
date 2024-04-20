3b2c2f84cdc507f8c2c29a235e882398d8bbc216
CLI Arena <git@cliarena.com>
2024-04-20 10:26:51 +0100

    fix(ssh): known hosts for GH & GL

--- modules/sshd.nix
+++ modules/sshd.nix
@@ -15,5 +15,12 @@
       AllowStreamLocalForwarding no
       AuthenticationMethods publickey
     '';
+    knownHosts = {
+      # This to avoid fingerprints verification prompt on git clone
+      "gitlab.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      "github.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
   };
 }
