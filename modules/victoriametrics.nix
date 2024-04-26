e1f53db80db9cc2e8c5734681d39f4611f3656f2
CLI Arena <git@cliarena.com>
2024-04-26 19:09:38 +0100

    feat(modules): added victoriametrics module

---
+++ modules/victoriametrics.nix
@@ -0,0 +1,8 @@
+{ ... }: {
+  services.victoriametrics = {
+    enable = true;
+    extraOptions = [ "-storageDataPath=/srv/victoriametrics" ];
+    listenAddress = ":8428";
+    retentionPeriod = 36;
+  };
+}
