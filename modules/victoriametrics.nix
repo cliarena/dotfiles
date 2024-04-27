88ee974fca49b0df2c2c784fb1c1591f39b738fe
CLI Arena <git@cliarena.com>
2024-04-27 17:05:43 +0100

    feat(victoriametircs): added nomad target

--- modules/victoriametrics.nix
+++ modules/victoriametrics.nix
@@ -5,4 +5,20 @@
     listenAddress = ":8428";
     retentionPeriod = 36;
   };
+  services.vmagent = {
+    enable = true;
+    prometheusConfig = builtins.toJSON {
+      scrape_configs = [{
+        job_name = "nomad";
+        nomad_sd_configs = [{ server = "localhost:4646"; }];
+      }
+      # server is an optional Nomad server to connect to.
+      # If the server isn't specified, then it is read from NOMAD_ADDR environment var.
+      # If the NOMAD_ADDR environment var isn't set, then localhost:4646 is used.
+      #
+        ];
+    };
+  };
+  # services.prometheus.exporters.node.enable = true;
+
 }
