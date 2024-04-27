70ffc85701a25820c6891b438518a78b1414cfe9
CLI Arena <git@cliarena.com>
2024-04-27 21:00:07 +0100

    fix(vmetrics): vmagent config

--- modules/victoriametrics.nix
+++ modules/victoriametrics.nix
@@ -7,16 +7,11 @@
   };
   services.vmagent = {
     enable = true;
-    prometheusConfig = builtins.toJSON {
-      scrapeConfigs = [{
-        jobName = "nomad";
-        nomadSdConfigs = [{ server = "localhost:4646"; }];
-      }
-      # server is an optional Nomad server to connect to.
-      # If the server isn't specified, then it is read from NOMAD_ADDR environment var.
-      # If the NOMAD_ADDR environment var isn't set, then localhost:4646 is used.
-      #
-        ];
+    prometheusConfig = {
+      scrape_configs = [{
+        job_name = "nomad";
+        nomad_sd_configs = [{ server = "localhost:4646"; }];
+      }];
     };
   };
   # services.prometheus.exporters.node.enable = true;
