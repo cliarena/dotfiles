3f6b11c5e852fa5909e9e066ccf4c7f78e818a4a
CLI Arena <git@cliarena.com>
2024-04-29 19:18:42 +0100

    feat(vmetrics): added more prometheus exporters

--- modules/victoriametrics.nix
+++ modules/victoriametrics.nix
@@ -1,10 +1,11 @@
 { ... }: {
   services.prometheus.exporters = {
+
+    ### Linux processes ###
     node.enable = true;
+    process.enable = false; # monitor specefic process
     systemd.enable = true;
-    snmp.enable = true;
-    smokeping.enable = true; # ping services
-    smartctl.enable = true; # HDD health monitor
+
     script = { # custom scripts checks
       enable = false;
       scripts = [{
@@ -13,6 +14,25 @@
         timeout = 5;
       }];
     };
+
+    ### Networking ###
+    snmp.enable = true;
+    smokeping.enable = true; # ping services
+
+    ### DB ###
+    sql.enable = true; # custom SQL queries
+    redis.enable = false;
+    postgres.enable = false;
+
+    ### Caching ###
+    varnish.enable = false; # reverse proxy Cache
+
+    ### Hardware ###
+    smartctl.enable = true; # HDD health monitor
+    scaphandre.enable = true; # Electricity consumption for bare metal & vms
+
+    ### Spam ###
+    rspamd.enable = false; # Email/messages spam fileterer
 
   };
 
