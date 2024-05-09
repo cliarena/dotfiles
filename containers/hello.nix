ddfbac9e3068ba0db6ce1db8e49f05568138d201
CLI Arena <git@cliarena.com>
2024-05-09 07:01:16 +0100

    Revert "feat(containers): added arg to hello container for testing"
    This reverts commit 75c38eed5f61b5c1c929344eb9df6f930cc99270.

--- containers/hello.nix
+++ containers/hello.nix
@@ -1,4 +1,4 @@
-{ inputs, nixpkgs, name ? "Tom", ... }:
+{ inputs, nixpkgs, ... }:
 let
 
   user = "x";
@@ -29,7 +29,7 @@ in {
         wantedBy = [ "multi-user.target" ];
         script = ''
           while true; do
-            echo hello ${name} | ${pkgs.netcat}/bin/nc -lN 50
+            echo hello | ${pkgs.netcat}/bin/nc -lN 50
           done
         '';
       };
