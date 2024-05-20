d4cabf6076318e1b47fcb71be77dcf37e353be35
CLI Arena <git@cliarena.com>
2024-05-20 19:45:32 +0100

    fix(consul): disabled consul api geteway registerer. not needed after migrating to terranix

--- modules/hashi_stack/consul/default.nix
+++ modules/hashi_stack/consul/default.nix
@@ -2,6 +2,6 @@
   imports = [
     ./main.nix
     # ./http_route_refresher.nix
-    ./api_gateway_regiterer.nix # -- TODO: look if its possible to update gateway config natively  to reduce dependency management to pin envoy compatibility with consul
+    # ./api_gateway_regiterer.nix
   ];
 }
