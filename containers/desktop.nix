0564da547da0eaf7908aff768cf2fd059ec0565c
CLI Arena <git@cliarena.com>
2024-08-06 13:11:42 +0100

    fix(containers): added amd hardware config to desktop container

--- containers/desktop.nix
+++ containers/desktop.nix
@@ -101,6 +101,7 @@ in {
       imports = [
         # ../modules/netwoking/container-network.nix
         # ../modules/boot/amd.nix
+        ../modules/hardware/amd.nix
         ../modules/hyprland.nix
 
         home-manager.nixosModules.home-manager
