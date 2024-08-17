e4b3030f202d9b94e82fe722a3bccfb4e8e28748
CLI Arena <git@cliarena.com>
2024-08-17 15:45:19 +0100

    fix(terra_nomad): removed all jobs & added nomoperator

--- terranix/nomad/default.nix
+++ terranix/nomad/default.nix
@@ -1,33 +1,21 @@
-{ nix-nomad, self, microvm, pkgs, config, ... }:
+{ nix-nomad, ... }:
 let
   jobs = nix-nomad.lib.mkNomadJobs {
     system = "x86_64-linux";
     # system = "aarch64-linux";
     config = [
-      ./kasm.nix
-      # ./echoip_nix.nix
-      ./echo.nix
-      ./wolf_nix.nix
-      ./nginx.nix
-      (import ./microvm.nix { inherit microvm self pkgs config; })
+      ./nomoperator.nix
+      # ./kasm.nix
+      # # ./echoip_nix.nix
+      # ./echo.nix
+      # ./wolf_nix.nix
+      # ./nginx.nix
+      # (import ./microvm.nix { inherit microvm self pkgs config; })
     ];
   };
 in {
-  resource.nomad_job.wolf = {
-    jobspec = ''''${file("${jobs}/wolf.json")}'';
+  resource.nomad_job.nomoperator = {
+    jobspec = ''''${file("${jobs}/nomoperator.json")}'';
     json = true;
   };
-  resource.nomad_job.nginx = {
-    jobspec = ''''${file("${jobs}/nginx.json")}'';
-    json = true;
-  };
-  resource.nomad_job.echo = {
-    jobspec = ''''${file("${jobs}/echo.json")}'';
-    json = true;
-  };
-  resource.nomad_job.microvm = {
-    jobspec = ''''${file("${jobs}/microvm.json")}'';
-    json = true;
-  };
-  # data.vault_kv_secret.test = { path = "kv/test"; };
 }
