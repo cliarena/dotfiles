ee96c609398c29b287f106e9cac9e91ebb636b26
CLI Arena <git@cliarena.com>
2024-08-17 11:41:41 +0100

    feat(terranix): added nomoperator job

---
+++ terranix/nomad/nomoperator.nix
@@ -0,0 +1,38 @@
+{ time, ... }: {
+  job.nomoperator = {
+    datacenters = [ "dc1" ];
+
+    group.servers = {
+      count = 1;
+
+      task.server = {
+        driver = "exec";
+
+        config = {
+          command = "nomoperator";
+          args = [
+            "bootstrap"
+            "git"
+            "--url"
+            "https://github.com/jonasvinther/nomad-state.git"
+            "--branch"
+            "main"
+            "--path"
+            "jobs/*.nomad"
+            # "./terranix/nomad/jobs/*.json"
+          ];
+        };
+        artifacts = [{
+          source =
+            "https://github.com/jonasvinther/nomad-gitops-operator/releases/download/v0.0.2/nomad-gitops-operator_0.0.2_linux_amd64.tar.gz";
+          destination = "local";
+          mode = "any";
+        }];
+        # resources = {
+        # cpu = 10;
+        # memory = 50;
+        # };
+      };
+    };
+  };
+}
