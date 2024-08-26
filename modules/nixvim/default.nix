8155ddd63927faf1f4a9f66167c310f412f212cd
CLI Arena <git@cliarena.com>
2024-08-26 11:15:12 +0100

    fix(nvim): missing lua-utils

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -81,6 +81,17 @@ in {
   ];
   extraPlugins = with pkgs.vimPlugins;
     [
+    (pkgs.vimUtils.buildVimPlugin {
+      inherit (pkgs.luaPackages.lua-utils-nvim) pname version src;
+    })
+
+    (pkgs.vimUtils.buildVimPlugin {
+      inherit (pkgs.luaPackages.pathlib-nvim) pname version src;
+    })
+
+    (pkgs.vimUtils.buildVimPlugin {
+      inherit (pkgs.luaPackages.nvim-nio) pname version src;
+    })
       popup-nvim # # Needed by some plugins
       plenary-nvim # # Lua Library
       nvim-tree-lua # # File Explorer
