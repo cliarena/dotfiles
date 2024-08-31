d48af6bc0e0b8fb2a3d4c41eee16bdf885a8b192
CLI Arena <git@cliarena.com>
2024-08-31 13:37:26 +0100

    feat(river): set default layout to rivertile

--- modules/home/river.nix
+++ modules/home/river.nix
@@ -2,7 +2,8 @@
   wayland.windowManager.river = {
     enable = true;
     settings = {
-      border-width = 2;
+      border-width = 1;
+      default-layout = "rivertile";
       declare-mode = [ "locked" "normal" "passthrough" ];
       map = { normal = { "Alt Q" = "close"; }; };
       set-repeat = "50 300";
