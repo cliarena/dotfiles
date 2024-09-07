ff41274b82b3e9ec886ef5425dfb3f60c1cef711
CLI Arena <git@cliarena.com>
2024-09-07 08:50:15 +0100

    fix(container_desktop): removed nushell as default shell since am not able to make it work with wolf

--- containers/desktop.nix
+++ containers/desktop.nix
@@ -92,7 +92,6 @@ in {
       '';
 
       services.getty.autologinUser = "x";
-      users.defaultUserShell = pkgs.nushell;
       users.users.x = {
         isNormalUser = true;
         initialPassword = "nixos";
