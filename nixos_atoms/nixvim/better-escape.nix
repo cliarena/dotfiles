7244694ceec62bfbaf02499be9601fb02dfe6e79
CLI Arena <git@cliarena.com>
2024-09-17 08:24:09 +0100

    refactor(nixvim): modulazed better-escape

--- nixos_atoms/nixvim/better-escape.nix
+++ nixos_atoms/nixvim/better-escape.nix
@@ -1,6 +1,21 @@
-{ ... }: {
-  plugins.better-escape.settings = {
-    default_mappings = false;
-    mappings = { i = { i = { i = "<Esc>"; }; }; };
+{ config, lib, ... }:
+let
+  module = "_better_escape";
+  deskription = "escape without delay";
+  inherit (lib) mkEnableOption mkIf;
+in {
+
+  options.${module}.enable = mkEnableOption deskription;
+
+  config = mkIf config.${module}.enable {
+
+    plugins.better-escape = {
+      enable = true;
+      settings = {
+        default_mappings = false;
+        mappings = { i = { i = { i = "<Esc>"; }; }; };
+      };
+    };
   };
+
 }
