47340e9c52ccfa56627afa8ed9e1115668f29359
CLI Arena <git@cliarena.com>
2024-09-20 19:00:57 +0100

    refactor(nvim): nixifiyed neotest

---
+++ nixos_atoms/nixvim/neotest.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+let
+  module = "_neotest";
+  deskription = "instant inline tests";
+  inherit (lib) mkEnableOption mkIf;
+in {
+
+  options.${module}.enable = mkEnableOption deskription;
+
+  config = mkIf config.${module}.enable {
+
+    plugins.neotest = {
+      enable = true;
+      adapters.zig.enable = true;
+      settings = {
+        summary.mappings = {
+          next_failed = "U";
+          output = "o";
+          prev_failed = "E";
+          run = "r";
+          run_marked = "R";
+        };
+      };
+    };
+
+  };
+}
