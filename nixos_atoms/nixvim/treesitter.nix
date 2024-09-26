2c0b76b920425b47791acf8040ed68df50202671
CLI Arena <git@cliarena.com>
2024-09-26 09:10:51 +0100

    refactor(nvim): nixified treesitter base config

---
+++ nixos_atoms/nixvim/treesitter.nix
@@ -0,0 +1,31 @@
+{ config, lib, ... }:
+let
+  module = "_treesitter";
+  deskription = "treesitter config";
+  inherit (lib) mkEnableOption mkIf;
+in {
+
+  options.${module}.enable = mkEnableOption deskription;
+
+  config = mkIf config.${module}.enable {
+
+    plugins.treesitter = {
+      enable = true;
+      folding = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = true;
+        incremental_selection = {
+          enable = true;
+          keymaps = {
+            init_selection = "<Leader>ss";
+            node_incremental = "<Leader>si";
+            scope_incremental = "<Leader>sc";
+            node_decremental = "<Leader>sd";
+          };
+        };
+      };
+    };
+
+  };
+}
