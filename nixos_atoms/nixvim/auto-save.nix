68c5e4efc603030a32c351fae333cec7505ae717
CLI Arena <git@cliarena.com>
2024-09-27 23:53:02 +0100

    refactor(nvim): modularized auto-save

--- nixos_atoms/nixvim/auto-save.nix
+++ nixos_atoms/nixvim/auto-save.nix
@@ -1,1 +1,16 @@
-{ ... }: { plugins.auto-save.enable = true; }
+{ config, lib, ... }:
+let
+  module = "_auto_save";
+  deskription = "auto save";
+  inherit (lib) mkEnableOption mkIf;
+in {
+
+  options.${module}.enable = mkEnableOption deskription;
+
+  config = mkIf config.${module}.enable {
+
+    programs.nixvim.plugins.auto-save.enable = true;
+
+  };
+
+}
