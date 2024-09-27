bce10328e423cdcdeb8642c698bed1b0e8b0532b
CLI Arena <git@cliarena.com>
2024-09-27 20:25:35 +0100

    refactor(nvim): nixified base navigation keymaps

---
+++ nixos_atoms/nixvim/keymaps.nix
@@ -0,0 +1,50 @@
+{ config, lib, ... }:
+let
+  module = "_keymaps";
+  deskription = "keymaps";
+  inherit (lib) mkEnableOption mkIf;
+in {
+
+  options.${module}.enable = mkEnableOption deskription;
+
+  config = mkIf config.${module}.enable {
+
+    keymaps = [
+      # Navigation
+      {
+        action = "<Down>";
+        key = "e";
+        options = {
+          silent = true;
+          nowait = true;
+        };
+      }
+      {
+        action = "<Up>";
+        key = "l";
+        options = {
+          silent = true;
+          nowait = true;
+        };
+      }
+      {
+        action = "<Right>";
+        key = "a";
+        options = {
+          silent = true;
+          nowait = true;
+        };
+      }
+      {
+        action = "<Left>";
+        key = "n";
+        options = {
+          silent = true;
+          nowait = true;
+        };
+      }
+    ];
+
+  };
+
+}
