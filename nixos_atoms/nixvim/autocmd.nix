fdc15351d2a32d5112e78af18df50e833f660add
CLI Arena <git@cliarena.com>
2024-09-27 23:35:52 +0100

    refactor(nvim): modularized auto commands config

--- nixos_atoms/nixvim/autocmd.nix
+++ nixos_atoms/nixvim/autocmd.nix
@@ -1,8 +1,20 @@
-{ ... }: {
-  autoCmd = [{
-    desc = "Auto-refresh buffer if file changed externally";
-    command = "if mode() != 'c' | checktime | endif";
-    event = [ "FocusGained" "BufEnter" "CursorHold" "CursorHoldI" ];
-    pattern = [ "*" ];
-  }];
+{ config, lib, ... }:
+let
+  module = "_auto_cmds";
+  deskription = "auto commands";
+  inherit (lib) mkEnableOption mkIf;
+in {
+
+  options.${module}.enable = mkEnableOption deskription;
+
+  config = mkIf config.${module}.enable {
+
+    programs.nixvim.autoCmd = [{
+      desc = "Auto-refresh buffer if file changed externally";
+      command = "if mode() != 'c' | checktime | endif";
+      event = [ "FocusGained" "BufEnter" "CursorHold" "CursorHoldI" ];
+      pattern = [ "*" ];
+    }];
+  };
+
 }
