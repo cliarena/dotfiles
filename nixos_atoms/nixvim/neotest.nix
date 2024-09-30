4164358e2dcd16e167f3432d9fb14ff9a059e5cc
CLI Arena <git@cliarena.com>
2024-09-30 18:46:21 +0100

    fix(nvim): pinned neotest-zig to old version. new version is not working

--- nixos_atoms/nixvim/neotest.nix
+++ nixos_atoms/nixvim/neotest.nix
@@ -1,8 +1,17 @@
-{ config, lib, ... }:
+{ config, lib, system, ... }:
 let
   module = "_neotest";
   deskription = "instant inline tests";
   inherit (lib) mkEnableOption mkIf;
+  old_pkgs = import (builtins.fetchGit {
+    # Descriptive name to make the store path easier to identify
+    name = "my-old-revision";
+    url = "https://github.com/NixOS/nixpkgs/";
+    ref = "refs/heads/nixpkgs-unstable";
+    rev = "0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb";
+  }) { inherit system; };
+
+  old_neotest-zig_pkg = old_pkgs.vimPlugins.neotest-zig;
 in {
 
   options.${module}.enable = mkEnableOption deskription;
@@ -14,6 +23,7 @@ in {
 
       adapters.zig = {
         enable = true;
+        package = old_neotest-zig_pkg;
         settings.dap.adapter = "lldb";
       };
 
