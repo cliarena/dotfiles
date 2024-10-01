366f01d15539efad81f7e864461e7a265a17de1d
CLI Arena <git@cliarena.com>
2024-10-01 09:20:19 +0100

    fix(nvim): unpinned neotest since nothing makes it work

--- nixos_atoms/nixvim/neotest.nix
+++ nixos_atoms/nixvim/neotest.nix
@@ -3,16 +3,16 @@ let
   module = "_neotest";
   deskription = "instant inline tests";
   inherit (lib) mkEnableOption mkIf;
-  old_pkgs = import (builtins.fetchGit {
-    # Descriptive name to make the store path easier to identify
-    name = "my-old-revision";
-    url = "https://github.com/NixOS/nixpkgs/";
-    ref = "refs/heads/nixpkgs-unstable";
-    rev = "05bbf675397d5366259409139039af8077d695ce";
-  }) { inherit system; };
-
-  old_neotest_pkg = old_pkgs.vimPlugins.neotest;
-  old_neotest-zig_pkg = old_pkgs.vimPlugins.neotest-zig;
+  # old_pkgs = import (builtins.fetchGit {
+  #   # Descriptive name to make the store path easier to identify
+  #   name = "my-old-revision";
+  #   url = "https://github.com/NixOS/nixpkgs/";
+  #   ref = "refs/heads/nixpkgs-unstable";
+  #   rev = "05bbf675397d5366259409139039af8077d695ce";
+  # }) { inherit system; };
+  #
+  # old_neotest_pkg = old_pkgs.vimPlugins.neotest;
+  # old_neotest-zig_pkg = old_pkgs.vimPlugins.neotest-zig;
 in {
 
   options.${module}.enable = mkEnableOption deskription;
@@ -21,7 +21,7 @@ in {
 
     programs.nixvim.plugins.neotest = {
       enable = true;
-      package = old_neotest_pkg;
+      # package = old_neotest_pkg;
 
       adapters.zig = {
         enable = true;
