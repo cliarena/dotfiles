f8518adbba1f3ae8d95bfb04b38755edd951b6c8
CLI Arena <git@cliarena.com>
2024-10-03 21:33:58 +0100

    feat(nvim): added more keymaps

--- nixos_atoms/nixvim/keymaps.nix
+++ nixos_atoms/nixvim/keymaps.nix
@@ -58,6 +58,36 @@ in {
         key = "glN";
         options = { desc = "Prev Diagnostic"; };
       }
+      {
+        key = "<leader>xx";
+        action = "<cmd>Trouble diagnostics toggle<cr>";
+        options.desc = "Diagnostics (Trouble)";
+      }
+      {
+        key = "<leader>xX";
+        action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+        options.desc = "Buffer Diagnostics (Trouble)";
+      }
+      {
+        key = "<leader>cs";
+        action = "<cmd>Trouble symbols toggle focus=false<cr>";
+        options.desc = "Symbols (Trouble)";
+      }
+      {
+        key = "<leader>cl";
+        action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+        options.desc = "LSP Definitions / references / ... (Trouble)";
+      }
+      {
+        key = "<leader>xL";
+        action = "<cmd>Trouble loclist toggle<cr>";
+        options.desc = "Location List (Trouble)";
+      }
+      {
+        key = "<leader>xQ";
+        action = "<cmd>Trouble qflist toggle<cr>";
+        options.desc = "Quickfix List (Trouble)";
+      }
     ];
 
   };
