1b498eff9da0f7a4069343d2191a6031c0d110ba
CLI Arena <git@cliarena.com>
2024-10-20 07:07:41 +0100

    feat(river): changed mode key & res & added dofus keymap

--- nixos_modules/river.nix
+++ nixos_modules/river.nix
@@ -5,15 +5,17 @@ let
   inherit (lib) mkEnableOption mkIf;
   inherit (inputs) home-manager;
 
+  mode = "Alt";
+  resolution = "3840x2160";
   terminal = "'${pkgs.kitty}/bin/kitty -e ${pkgs.nushell}/bin/nu'";
 
   tags = builtins.genList (x: x) 9;
   tag_map_list = builtins.map (tag: {
-    "Super ${toString (tag + 1)}" = "set-focused-tags ${toString tag}";
-    "Super+Shift ${toString (tag + 1)}" = "set-view-tags ${toString tag}";
-    "Super+Control ${toString (tag + 1)}" =
+    "${mode} ${toString (tag + 1)}" = "set-focused-tags ${toString tag}";
+    "${mode}+Shift ${toString (tag + 1)}" = "set-view-tags ${toString tag}";
+    "${mode}+Control ${toString (tag + 1)}" =
       "toggle-focused-tags ${toString tag}";
-    "Super+Shift+Control ${toString (tag + 1)}" =
+    "${mode}+Shift+Control ${toString (tag + 1)}" =
       "toggle-view-tags ${toString tag}";
   }) tags;
 
@@ -36,19 +38,21 @@ in {
           declare-mode = [ "locked" "normal" "passthrough" ];
           map = {
             normal = {
-              "Super Q" = "close";
-              "Super+Shift E" = "exit";
-              "Super F" = "toggle-fullscreen";
-              "Super F11" = "enter-mode passthrough";
-              "Super Return" = "spawn ${terminal}";
+              "${mode} Q" = "close";
+              "${mode}+Shift E" = "exit";
+              "${mode} F" = "toggle-fullscreen";
+              "${mode} F11" = "enter-mode passthrough";
+
+              "${mode} Return" = "spawn ${terminal}";
+              "${mode} D" = "spawn ankama-launcher";
             } // tag_map;
-            passthrough = { "Super F11" = "enter-mode normal"; };
+            passthrough = { "${mode} F11" = "enter-mode normal"; };
           };
           set-repeat = "50 300";
           spawn = [
             "${pkgs.brave}/bin/brave"
             terminal
-            "'${pkgs.wlr-randr}/bin/wlr-randr --output WL-1 --custom-mode 1920x1080'"
+            "'${pkgs.wlr-randr}/bin/wlr-randr --output WL-1 --custom-mode ${resolution}'"
             "'${pkgs.river}/bin/rivertile -view-padding 1 -outer-padding 3'"
             "${pkgs.swww}/bin/swww-daemon"
             "'${pkgs.coreutils}/bin/shuf -zen1 /srv/wallpapers/* | ${pkgs.findutils}/bin/xargs -0 ${pkgs.swww}/bin/swww img'"
