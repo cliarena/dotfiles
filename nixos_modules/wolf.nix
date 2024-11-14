4ef10bb830ef3cd7894b8c100b385153d3f87fed
CLI Arena <git@cliarena.com>
2024-11-14 19:49:15 +0100

    feat(wolf): added as a service

--- nixos_modules/wolf.nix
+++ nixos_modules/wolf.nix
@@ -3,6 +3,8 @@ let
   module = "_wolf";
   description = "stream desktops containers";
   inherit (lib) mkEnableOption mkIf;
+  wolf_bin = "${inputs.wolf.packages.x86_64-linux.default}/bin/wolf";
+
 in {
 
   options.${module}.enable = mkEnableOption description;
@@ -10,17 +12,17 @@ in {
   config = mkIf config.${module}.enable {
 
     environment.systemPackages = [ inputs.wolf.packages.x86_64-linux.default ];
-    # systemd.services.wolf = {
-    #   path = [ inputs.wolf.packages.x86_64-linux.default ];
-    #   description = "stream desktops containers";
-    #   # environment = { VAULT_ADDR = "https://vault.cliarena.com:8200"; };
-    #   script = "ls && wolf";
-    #   serviceConfig = {
-    #     Restart = "on-failure";
-    #     # avoid error start request repeated too quickly since RestartSec defaults to 100ms
-    #     RestartSec = 3;
-    #   };
-    #   wantedBy = [ "multi-user.target" ];
-    # };
+    systemd.services.wolf = {
+      # path = [ inputs.wolf.packages.x86_64-linux.default ];
+      description = "stream desktops containers";
+      # environment = { VAULT_ADDR = "https://vault.cliarena.com:8200"; };
+      script = wolf_bin;
+      serviceConfig = {
+        Restart = "on-failure";
+        # avoid error start request repeated too quickly since RestartSec defaults to 100ms
+        RestartSec = 3;
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
