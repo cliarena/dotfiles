e2b661e9bd53184bd86384de4c26498490fd4dfe
CLI Arena <git@cliarena.com>
2024-12-03 19:53:26 +0100

    fix(ankama): launcher throws an error when launching

--- nixos_modules/ankama.nix
+++ nixos_modules/ankama.nix
@@ -27,11 +27,11 @@ in {
         (appimageTools.wrapType2 {
           inherit name src;
 
-          extraInstallCommands = ''
-            install -m 444 -D ${appimageContents}/zaap.desktop $out/share/applications/ankama-launcher.desktop
-            sed -i 's/.*Exec.*/Exec=ankama-launcher/' $out/share/applications/ankama-launcher.desktop
-            install -m 444 -D ${appimageContents}/zaap.png $out/share/icons/hicolor/256x256/apps/zaap.png
-          '';
+          # extraInstallCommands = ''
+          #   install -m 444 -D ${appimageContents}/zaap.desktop $out/share/applications/ankama-launcher.desktop
+          #   sed -i 's/.*Exec.*/Exec=ankama-launcher/' $out/share/applications/ankama-launcher.desktop
+          #   install -m 444 -D ${appimageContents}/zaap.png $out/share/icons/hicolor/256x256/apps/zaap.png
+          # '';
         })
 
       ];
