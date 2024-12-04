c509486b78c604c0094eacf1ee3446288a016e56
CLI Arena <git@cliarena.com>
2024-12-04 16:06:13 +0100

    fix(ankama): moved back to the older launcher

--- nixos_modules/ankama.nix
+++ nixos_modules/ankama.nix
@@ -6,14 +6,16 @@ let
 
   name = "ankama-launcher";
   src = builtins.fetchurl {
-    # url = "https://download.ankama.com/launcher/full/linux/x64";
-    # "https://launcher.cdn.ankama.com/installers/production/Ankama+Launcher-Setup-x86_64.AppImage";
-    url =
-      "https://launcher.cdn.ankama.com/installers/production/Dofus_3.0-x86_64.AppImage";
-    # To get sha256: nix-prefetch-url <url>
-    sha256 = "16vyw20sq632nshm4fj11mv7qx6k6nwbjk9hbkcmiq1m45i1r766";
-    # "00mag1kjvqcxhs50fwvjyw5wvrkn8lagbr1lzkqg8rgac1qzb6p3";
     name = "ankama-launcher.AppImage";
+
+    # Old
+    url =
+      "https://launcher.cdn.ankama.com/installers/production/Ankama+Launcher-Setup-x86_64.AppImage";
+    sha256 = "00mag1kjvqcxhs50fwvjyw5wvrkn8lagbr1lzkqg8rgac1qzb6p3";
+
+    # To get sha256: nix-prefetch-url <url>
+    # url = "https://launcher.cdn.ankama.com/installers/production/Dofus_3.0-x86_64.AppImage";
+    # sha256 = "16vyw20sq632nshm4fj11mv7qx6k6nwbjk9hbkcmiq1m45i1r766";
   };
 
   appimageContents = pkgs.appimageTools.extractType2 { inherit name src; };
@@ -27,11 +29,11 @@ in {
         (appimageTools.wrapType2 {
           inherit name src;
 
-          # extraInstallCommands = ''
-          #   install -m 444 -D ${appimageContents}/zaap.desktop $out/share/applications/ankama-launcher.desktop
-          #   sed -i 's/.*Exec.*/Exec=ankama-launcher/' $out/share/applications/ankama-launcher.desktop
-          #   install -m 444 -D ${appimageContents}/zaap.png $out/share/icons/hicolor/256x256/apps/zaap.png
-          # '';
+          extraInstallCommands = ''
+            install -m 444 -D ${appimageContents}/zaap.desktop $out/share/applications/ankama-launcher.desktop
+            sed -i 's/.*Exec.*/Exec=ankama-launcher/' $out/share/applications/ankama-launcher.desktop
+            install -m 444 -D ${appimageContents}/zaap.png $out/share/icons/hicolor/256x256/apps/zaap.png
+          '';
         })
 
       ];
