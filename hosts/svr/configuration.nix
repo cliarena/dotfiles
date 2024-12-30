b37e3db13061107542219a1e0e2bb550e156d07d
CLI Arena <git@cliarena.com>
2024-12-30 11:05:50 +0100

    fix(opensplat): rocm support

--- hosts/svr/configuration.nix
+++ hosts/svr/configuration.nix
@@ -60,32 +60,33 @@ in {
     ### Virtualization ###
     virtiofsd # needed by microvm jobs to use virtiofs shares
 
-    (opensplat.overrideAttrs (finalAttrs: previousAttrs: {
-      # env.PYTORCH_ROCM_ARCH =
-      # "gfx900;gfx906;gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx940;gfx941;gfx942";
+    (opensplat.overrideAttrs (finalAttrs: previousAttrs:
+      {
+        # env.PYTORCH_ROCM_ARCH =
+        # "gfx900;gfx906;gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx940;gfx941;gfx942";
 
-      # buildInputs = previousAttrs.buildInputs ++ rocm_pkgs;
-      # ++ (with pkgs.rocmPackages; [ rocblas hipblas clr ]);
-      # [ python311Packages.torchWithRocm ];
+        # buildInputs = previousAttrs.buildInputs ++ rocm_pkgs;
+        # ++ (with pkgs.rocmPackages; [ rocblas hipblas clr ]);
+        # [ python311Packages.torchWithRocm ];
 
-      cmakeFlags = previousAttrs.cmakeFlags ++ [
-        (lib.cmakeFeature "GPU_RUNTIME" "HIP")
-        # (lib.cmakeFeature "HIP_DIR" "/opt/rocm")
-        # (lib.cmakeFeature "HIP_ROOT_DIR" "${rocmEnv}/lib")
-        (lib.cmakeFeature "OPENSPLAT_BUILD_SIMPLE_TRAINER" "ON")
-        # (lib.cmakeFeature "CMAKE_MODULE_PATH" "/opt/rocm/lib/cmake/hip")
-        # (lib.cmakeFeature "CMAKE_MODULE_PATH" "${rocmEnv}/lib/cmake/hip")
-        # (lib.cmakeFeature "CMAKE_PREFIX_PATH" "${rocmEnv}/lib/cmake")
-        (lib.cmakeFeature "CMAKE_PREFIX_PATH" "${rocmEnv}/lib/cmake")
-        # (lib.cmakeFeature "CMAKE_HIP_COMPILER_ROCM_ROOT" "${rocmEnv}")
-        # (lib.cmakeFeature "CMAKE_HIP_COMPILER" "${rocmEnv}/lib/cmake/hip")
-        # TODO: auto-detect
-        (lib.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx1032;gfx90c:xnack-")
-        # (lib.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx000;gfx1032;gfx90c")
-        # "gfx900;gfx906;gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx940;gfx941;gfx942")
-
-      ];
-    }))
+        # cmakeFlags = previousAttrs.cmakeFlags ++ [
+        #   (lib.cmakeFeature "GPU_RUNTIME" "HIP")
+        #   # (lib.cmakeFeature "HIP_DIR" "/opt/rocm")
+        #   # (lib.cmakeFeature "HIP_ROOT_DIR" "${rocmEnv}/lib")
+        #   (lib.cmakeFeature "OPENSPLAT_BUILD_SIMPLE_TRAINER" "ON")
+        #   # (lib.cmakeFeature "CMAKE_MODULE_PATH" "/opt/rocm/lib/cmake/hip")
+        #   # (lib.cmakeFeature "CMAKE_MODULE_PATH" "${rocmEnv}/lib/cmake/hip")
+        #   # (lib.cmakeFeature "CMAKE_PREFIX_PATH" "${rocmEnv}/lib/cmake")
+        #   (lib.cmakeFeature "CMAKE_PREFIX_PATH" "${rocmEnv}/lib/cmake")
+        #   # (lib.cmakeFeature "CMAKE_HIP_COMPILER_ROCM_ROOT" "${rocmEnv}")
+        #   # (lib.cmakeFeature "CMAKE_HIP_COMPILER" "${rocmEnv}/lib/cmake/hip")
+        #   # TODO: auto-detect
+        #   (lib.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx1032;gfx90c:xnack-")
+        #   # (lib.cmakeFeature "CMAKE_HIP_ARCHITECTURES" "gfx000;gfx1032;gfx90c")
+        #   # "gfx900;gfx906;gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx940;gfx941;gfx942")
+        #
+        # ];
+      }))
 
     colmap
   ];
