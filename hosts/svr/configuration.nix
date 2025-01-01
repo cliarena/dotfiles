11e0e5b3c5ad8312630d9e98024d5db5a93bdb03
CLI Arena <git@cliarena.com>
2025-01-01 11:35:05 +0100

    ..

--- hosts/svr/configuration.nix
+++ hosts/svr/configuration.nix
@@ -104,7 +104,7 @@ in {
         export ROCM_PATH=${rocm_toolkit}
         export ROCM_SOURCE_DIR=${rocm_toolkit}
         export PYTORCH_ROCM_ARCH="${gpuTargetString}"
-        # export CMAKE_CXX_FLAGS="-I${rocm_toolkit}/include -I${rocm_toolkit}/include/rocblas"
+        export CMAKE_CXX_FLAGS="-I${rocm_toolkit}/include -I${rocm_toolkit}/include/rocblas"
       '';
       # dontUseCmakeConfigure = true;
 
