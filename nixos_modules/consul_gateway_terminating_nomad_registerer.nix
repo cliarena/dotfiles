3fec94046497e92cf6ff6ada6f3fd178cd1828f8
CLI Arena <git@cliarena.com>
2025-02-03 16:14:04 +0100

    ..

--- nixos_modules/consul_gateway_terminating_nomad_registerer.nix
+++ nixos_modules/consul_gateway_terminating_nomad_registerer.nix
@@ -32,7 +32,8 @@ in {
       path = [ pkgs.getent pkgs.envoy ];
       description = "register consul nomad terminating gateway";
       script = ''
-        ${pkgs.consul}/bin/consul connect envoy -gateway terminating -register -service nomad-client-gateway -ignore-envoy-compatibility
+        ${pkgs.consul}/bin/consul connect envoy -sidecar-for nomad-client -bootstrap -ignore-envoy-compatibility
+        # ${pkgs.consul}/bin/consul connect envoy -gateway terminating -register -service nomad-client-gateway -ignore-envoy-compatibility
       '';
       serviceConfig = {
         Restart = "on-failure";
