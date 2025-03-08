e044a14f2ae27d86cabe0f1b23569b98ffbfe667
CLI Arena <git@cliarena.com>
2025-03-08 15:54:59 +0000

    ..

--- nixos_atoms/nixvim/options.nix
+++ nixos_atoms/nixvim/options.nix
@@ -19,7 +19,7 @@ in {
       globals.loaded_netrwPlugin = 1;
       globals.editorconfig = false;
       # globals.loaded_man = 1;
-      # globals.loaded_matchit = 1;
+      globals.loaded_matchit = 1;
       # globals.loaded_matchparen = 1;
       # globals.loaded_remote_plugins = 1;
       globals.loaded_shada_plugin = 1;
