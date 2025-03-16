ff693b8a0972d415d8522c90600d3c44fba4faf4
CLI Arena <git@cliarena.com>
2025-03-16 02:24:09 +0000

    fix(home): wezterm config

--- nixos_modules/home_wezterm.nix
+++ nixos_modules/home_wezterm.nix
@@ -1,29 +1,34 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, inputs, host, ... }:
 let
   module = "_home_wezterm";
   description = "wezterm config";
   inherit (lib) mkEnableOption mkIf;
+  inherit (inputs) home-manager;
 in {
+
+  imports = [ home-manager.nixosModules.home-manager ];
 
   options.${module}.enable = mkEnableOption description;
 
   config = mkIf config.${module}.enable {
-    programs.wezterm = {
-      enable = true;
 
-      extraConfig = ''
-        return {
-          font_size = 9.0,
-          color_scheme = 'Catppuccin Mocha',
-          use_fancy_tab_bar = false,
-          hide_tab_bar_if_only_one_tab = true,
-          # default_prog = { "zsh", "--login", "-c", "tmux attach -t dev || tmux new -s dev" },
-          keys = {
-            {key="n", mods="SHIFT|CTRL", action="ToggleFullScreen"},
+    home-manager.users.${host.user} = {
+      programs.wezterm = {
+        enable = true;
+
+        extraConfig = ''
+          return {
+            font_size = 9.0,
+            color_scheme = 'Catppuccin Mocha',
+            use_fancy_tab_bar = false,
+            hide_tab_bar_if_only_one_tab = true,
+            # default_prog = { "zsh", "--login", "-c", "tmux attach -t dev || tmux new -s dev" },
+            keys = {
+              {key="n", mods="SHIFT|CTRL", action="ToggleFullScreen"},
+            }
           }
-        }
-      '';
+        '';
+      };
     };
-
   };
 }
