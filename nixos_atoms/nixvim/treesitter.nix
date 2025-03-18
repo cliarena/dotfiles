ef4d8e8788507e714d9d302db2e416996628e57c
CLI Arena <git@cliarena.com>
2025-03-18 03:46:40 +0000

    feat(treesitter): added hmts to highlight home-manager string configs

--- nixos_atoms/nixvim/treesitter.nix
+++ nixos_atoms/nixvim/treesitter.nix
@@ -9,23 +9,26 @@ in {
 
   config = mkIf config.${module}.enable {
 
-    programs.nixvim.plugins.treesitter = {
-      enable = true;
-      folding = true;
-      settings = {
-        highlight.enable = true;
-        indent.enable = true;
-        incremental_selection = {
-          enable = true;
-          keymaps = {
-            init_selection = "<Leader>ss";
-            node_incremental = "<Leader>si";
-            scope_incremental = "<Leader>sc";
-            node_decremental = "<Leader>sd";
+    programs.nixvim = {
+      plugins.hmts.enable = true; # highlight home-manager strings
+      plugins.treesitter = {
+        enable = true;
+        folding = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+          incremental_selection = {
+            enable = true;
+            keymaps = {
+              init_selection = "<Leader>ss";
+              node_incremental = "<Leader>si";
+              scope_incremental = "<Leader>sc";
+              node_decremental = "<Leader>sd";
+            };
           };
         };
       };
-    };
 
+    };
   };
 }
