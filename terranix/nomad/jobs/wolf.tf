a645c9739fb4031cbaadbd7338a2c668a4b95509
CLI Arena <git@cliarena.com>
2025-03-18 18:10:05 +0000

    fix(nomad.jobs.wolf): forgot to expose control port

--- terranix/nomad/jobs/wolf.tf
+++ terranix/nomad/jobs/wolf.tf
@@ -64,7 +64,7 @@ job "wolf" {
       }
 
       config {
-        ports = ["http", "https", "rtsp", "audio_0", "audio_1", "audio_2", "audio_3", "video_0", "video_1", "video_2", "video_3", ]
+        ports = ["http", "https", "rtsp", "control", "audio_0", "audio_1", "audio_2", "audio_3", "video_0", "video_1", "video_2", "video_3", ]
 
         volumes = [
           "wolf/:/etc/wolf",
