9d68b376ddc67c1002672cedc0d692541e1260b5
CLI Arena <git@cliarena.com>
2025-03-18 17:01:07 +0000

    fix(nomad.jobs): wolf missconfigured

--- terranix/nomad/jobs/wolf.tf
+++ terranix/nomad/jobs/wolf.tf
@@ -14,26 +14,26 @@ job "wolf" {
         XDG_RUNTIME_DIR        = "/tmp/sockets"
         HOST_APPS_STATE_FOLDER = "/srv/volumes/wolf"
       }
-      volumes = [
-        "wolf/:/etc/wolf",
-        "/tmp/sockets:/tmp/sockets:rw",
-        "/var/run/docker.sock:/var/run/docker.sock:rw",
-        "/dev/:/dev/:rw",
-        "/run/udev:/run/udev:rw",
-      ]
-      devices = [
-        {
-          host_path = "/dev/dri"
-        },
-        {
-          host_path = "/dev/uinput"
-        },
-        {
-          host_path = "/dev/uhid"
-        }
-      ]
 
       config {
+        volumes = [
+          "wolf/:/etc/wolf",
+          "/tmp/sockets:/tmp/sockets:rw",
+          "/var/run/docker.sock:/var/run/docker.sock:rw",
+          "/dev/:/dev/:rw",
+          "/run/udev:/run/udev:rw",
+        ]
+        devices = [
+          {
+            host_path = "/dev/dri"
+          },
+          {
+            host_path = "/dev/uinput"
+          },
+          {
+            host_path = "/dev/uhid"
+          }
+        ]
         image = "ghcr.io/games-on-whales/wolf:stable"
       }
     }
