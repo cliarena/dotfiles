a99df08b7434711634402b65eaa9480112a4ab5e
CLI Arena <git@cliarena.com>
2025-03-18 16:09:34 +0000

    feat(nomad.jobs): added wolf

---
+++ terranix/nomad/jobs/wolf.tf
@@ -0,0 +1,41 @@
+job "wolf" {
+
+  datacenters = [
+    "dc1"
+  ]
+  type = "service"
+
+  group "backend" {
+    count = 1
+
+    task "server" {
+      driver = "docker"
+      env {
+        XDG_RUNTIME_DIR        = "/tmp/sockets"
+        HOST_APPS_STATE_FOLDER = "/srv/volumes/wolf"
+      }
+      volumes = [
+        "wolf/:/etc/wolf",
+        "/tmp/sockets:/tmp/sockets:rw",
+        "/var/run/docker.sock:/var/run/docker.sock:rw",
+        "/dev/:/dev/:rw",
+        "/run/udev:/run/udev:rw",
+      ]
+      devices = [
+        {
+          host_path = "/dev/dri"
+        },
+        {
+          host_path = "/dev/uinput"
+        },
+        {
+          host_path = "/dev/uhid"
+        }
+      ]
+
+      config {
+        image = "ghcr.io/games-on-whales/wolf:stable"
+      }
+    }
+  }
+}
