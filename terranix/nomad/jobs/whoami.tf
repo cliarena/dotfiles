7eb31de920c30ad6d99f8499e636703330e70381
CLI Arena <git@cliarena.com>
2025-03-18 15:20:50 +0000

    fmt(nomad.jobs): whoamin

--- terranix/nomad/jobs/whoami.tf
+++ terranix/nomad/jobs/whoami.tf
@@ -1,18 +1,18 @@
-   job "whoami" {
+job "whoami" {
 
   datacenters = [
-  "dc1"
-]
+    "dc1"
+  ]
   type = "service"
 
-    group "backend" {
-      count = 4
+  group "backend" {
+    count = 4
 
-        task "whoami" {
-            driver = "docker"
-            config {
-            image = "containous/whoami"
-            }
-        }
+    task "whoami" {
+      driver = "docker"
+      config {
+        image = "containous/whoami"
+      }
     }
+  }
 }
