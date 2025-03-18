255301c5022ce6928082d08fe8b1aa13615e6a16
CLI Arena <git@cliarena.com>
2025-03-18 17:56:39 +0000

    fix(nomad.jobs.wolf): configured ports

--- terranix/nomad/jobs/wolf.tf
+++ terranix/nomad/jobs/wolf.tf
@@ -10,12 +10,50 @@ job "wolf" {
     count = 1
 
     network {
-      port "http" {}
-      port "https" {}
-      port "rtsp" {}
-      port "video" {}
-      port "audio" {}
+
+      port "http" {
+        static = 47989
+      }
+      port "https" {
+        static = 47984
+      }
+      port "control" {
+        static = 47999
+      }
+      port "rtsp" {
+        static = 48010
+      }
+
+      # audio port for each user
+      port "audio_0" {
+        static = 48200
+      }
+      port "audio_1" {
+        static = 48201
+      }
+      port "audio_2" {
+        static = 48202
+      }
+      port "audio_3" {
+        static = 48203
+      }
+
+      # video port for each user
+      port "video_0" {
+        static = 48100
+      }
+      port "video_1" {
+        static = 48101
+      }
+      port "video_2" {
+        static = 48102
+      }
+      port "video_3" {
+        static = 48103
+      }
+
     }
+
 
     task "server" {
       driver = "docker"
@@ -26,7 +64,7 @@ job "wolf" {
       }
 
       config {
-        ports = ["http", "https", "rtsp", "video", "audio"]
+        ports = ["http", "https", "rtsp", "audio_0", "audio_1", "audio_2", "audio_3", "video_0", "video_1", "video_2", "video_3", ]
 
         volumes = [
           "wolf/:/etc/wolf",
