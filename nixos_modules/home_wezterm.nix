6285a17bef9b61d61cbe838784ba48aee0ae00a5
CLI Arena <git@cliarena.com>
2025-03-18 02:28:05 +0000

    fix(nix): no highlighters for code blocks

--- nixos_modules/home_wezterm.nix
+++ nixos_modules/home_wezterm.nix
@@ -16,85 +16,86 @@ in {
       programs.wezterm = {
         enable = true;
 
-        extraConfig = ''
-          return {
-            line_height = 1.2,
-            font_size = 8.7, -- 8.6 fixes ===
+        extraConfig = # lua
+          ''
+            return {
+              line_height = 1.2,
+              font_size = 8.7, -- 8.6 fixes ===
 
-            bold_brightens_ansi_colors = "BrightAndBold",
-            color_scheme = 'Catppuccin Mocha',
+              bold_brightens_ansi_colors = "BrightAndBold",
+              color_scheme = 'Catppuccin Mocha',
 
-            window_padding = {
-              left = "0.3cell",
-              right = "0.3cell",
-              top = "0.3cell",
-              bottom = 0,
-            },
+              window_padding = {
+                left = "0.3cell",
+                right = "0.3cell",
+                top = "0.3cell",
+                bottom = 0,
+              },
 
-            tab_bar_at_bottom = true,
-            use_fancy_tab_bar = false,
-            hide_tab_bar_if_only_one_tab = true,
+              tab_bar_at_bottom = true,
+              use_fancy_tab_bar = false,
+              hide_tab_bar_if_only_one_tab = true,
 
-            default_prog = { "nu" },
+              default_prog = { "nu" },
 
-            keys = {
-            -- Create a new workspace with a random name and switch to it
-             {key="n", mods="SHIFT|CTRL", action="ToggleFullScreen"},
-            -- { key = 'i', mods = 'CTRL|SHIFT', action = SwitchToWorkspace },
-              -- Switch to the default workspace
-            -- {
-            --   key = 'd',
-            --   mods = 'CTRL|SHIFT',
-            --   action = "SwitchToWorkspace {
-            --     name = 'default',
-            --   }"
-            -- },
-            -- -- Switch to a monitoring workspace, which will have `top` launched into it
-            -- {
-            --   key = 'm',
-            --   mods = 'CTRL|SHIFT',
-            --   action = SwitchToWorkspace {
-            --     name = 'monitoring',
-            --     spawn = {
-            --       args = { 'btm' },
-            --     },
-            --   },
-            -- },
-            -- {
-            --   key = 'n',
-            --   mods = 'CTRL|SHIFT',
-            --   action = SwitchToWorkspace {
-            --     name = 'notes',
-            --     spawn = {
-            --       cwd = '~/notes',
-            --       args = { 'nvim' },
-            --     },
-            --   },
-            -- },
-            -- {
-            --   key = 'd',
-            --   mods = 'CTRL|SHIFT',
-            --   action = SwitchToWorkspace {
-            --     name = 'dotfiles',
-            --     spawn = {
-            --       cwd = '~/nixos',
-            --       args = { 'nvim' },
-            --     },
-            --   },
-            -- },
-            -- -- Show the launcher in fuzzy selection mode and have it list all workspaces
-            -- -- and allow activating one.
-            -- {
-            --   key = '9',
-            --   mods = 'ALT',
-            --   action = ShowLauncherArgs {
-            --     flags = 'FUZZY|WORKSPACES',
-            --   },
-            -- },
+              keys = {
+              -- Create a new workspace with a random name and switch to it
+               {key="n", mods="SHIFT|CTRL", action="ToggleFullScreen"},
+              -- { key = 'i', mods = 'CTRL|SHIFT', action = SwitchToWorkspace },
+                -- Switch to the default workspace
+              -- {
+              --   key = 'd',
+              --   mods = 'CTRL|SHIFT',
+              --   action = "SwitchToWorkspace {
+              --     name = 'default',
+              --   }"
+              -- },
+              -- -- Switch to a monitoring workspace, which will have `top` launched into it
+              -- {
+              --   key = 'm',
+              --   mods = 'CTRL|SHIFT',
+              --   action = SwitchToWorkspace {
+              --     name = 'monitoring',
+              --     spawn = {
+              --       args = { 'btm' },
+              --     },
+              --   },
+              -- },
+              -- {
+              --   key = 'n',
+              --   mods = 'CTRL|SHIFT',
+              --   action = SwitchToWorkspace {
+              --     name = 'notes',
+              --     spawn = {
+              --       cwd = '~/notes',
+              --       args = { 'nvim' },
+              --     },
+              --   },
+              -- },
+              -- {
+              --   key = 'd',
+              --   mods = 'CTRL|SHIFT',
+              --   action = SwitchToWorkspace {
+              --     name = 'dotfiles',
+              --     spawn = {
+              --       cwd = '~/nixos',
+              --       args = { 'nvim' },
+              --     },
+              --   },
+              -- },
+              -- -- Show the launcher in fuzzy selection mode and have it list all workspaces
+              -- -- and allow activating one.
+              -- {
+              --   key = '9',
+              --   mods = 'ALT',
+              --   action = ShowLauncherArgs {
+              --     flags = 'FUZZY|WORKSPACES',
+              --   },
+              -- },
+              }
+
             }
-
-          }
-        '';
+          '';
       };
     };
   };
