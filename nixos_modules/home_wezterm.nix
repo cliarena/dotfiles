22a662218c1cd33ca1829f9212b68c4805d40db1
CLI Arena <git@cliarena.com>
2025-03-19 08:20:55 +0000

    feat(wezterm): added spart_splits integration config

--- nixos_modules/home_wezterm.nix
+++ nixos_modules/home_wezterm.nix
@@ -19,6 +19,40 @@ in {
         extraConfig = # lua
           ''
             local act = wezterm.action;
+
+            local function is_vim(pane)
+              -- this is set by the plugin, and unset on ExitPre in Neovim
+              return pane:get_user_vars().IS_NVIM == 'true'
+            end
+
+            local direction_keys = {
+              h = 'Left',
+              j = 'Down',
+              k = 'Up',
+              l = 'Right',
+            }
+
+            local function split_nav(resize_or_move, key)
+              return {
+                key = key,
+                mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+                action = wezterm.action_callback(function(win, pane)
+                  if is_vim(pane) then
+                    -- pass the keys through to vim/nvim
+                    win:perform_action({
+                      SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+                    }, pane)
+                  else
+                    if resize_or_move == 'resize' then
+                      win:perform_action({ act.AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+                    else
+                      win:perform_action({ act.ActivatePaneDirection = direction_keys[key] }, pane)
+                    end
+                  end
+                end),
+              }
+            end
+
             return {
               line_height = 1.2,
               font_size = 8.7, -- 8.6 fixes ===
@@ -43,6 +77,17 @@ in {
               -- Create a new workspace with a random name and switch to it
                {key="n", mods="SHIFT|CTRL", action="ToggleFullScreen"},
                { key = 'i', mods = 'CTRL|SHIFT', action = act.SwitchToWorkspace },
+
+                -- move between split panes
+                split_nav('move', 'h'),
+                split_nav('move', 'j'),
+                split_nav('move', 'k'),
+                split_nav('move', 'l'),
+                -- resize panes
+                split_nav('resize', 'h'),
+                split_nav('resize', 'j'),
+                split_nav('resize', 'k'),
+                split_nav('resize', 'l'),
                 -- Switch to the default workspace
               -- {
               --   key = 'd',
