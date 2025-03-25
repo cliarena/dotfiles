6d2b3407d30571a2f0fd203b25aa23dd6748302c
CLI Arena <git@cliarena.com>
2025-03-25 16:01:52 +0000

    fix(ssh): fingerprints prompt on git clone

--- nixos_modules/ssh.nix
+++ nixos_modules/ssh.nix
@@ -1,4 +1,4 @@
-{ config, lib, inputs, host, ... }:
+{ config, lib, inputs, host, pkgs, ... }:
 let
   module = "_ssh_home";
   description = "ssh client config";
@@ -11,43 +11,54 @@ in {
 
   config = mkIf config.${module}.enable {
 
-    home-manager.users.${host.user} = { config, ... }: {
-      programs.ssh = {
-        enable = true;
-        matchBlocks = {
-          pi = {
-            host = "PI";
-            hostname = "10.10.0.2";
-            user = "pi";
-            port = 4729;
-            identityFile = config.sops.secrets.PI_SSH_KEY.path;
+    home-manager.users.${host.user} = { config, ... }:
+      let inherit (config.home) homeDirectory;
+      in {
+        programs.ssh = {
+          enable = true;
+          matchBlocks = {
+            pi = {
+              host = "PI";
+              hostname = "10.10.0.2";
+              user = "pi";
+              port = 4729;
+              identityFile = config.sops.secrets.PI_SSH_KEY.path;
+            };
+            svr = {
+              host = "SVR";
+              hostname = "10.10.2.1";
+              user = "svr";
+              port = 6523;
+              identityFile = config.sops.secrets.SVR_SSH_KEY.path;
+            };
+            space-x = {
+              host = "DS";
+              hostname = "10.10.2.1";
+              user = "x";
+              port = 10001;
+              identityFile = config.sops.secrets.DS_SSH_KEY.path;
+            };
+            gitlab = {
+              host = "gitlab.com";
+              identityFile = config.sops.secrets.GL_SSH_KEY.path;
+              extraOptions = { PreferredAuthentications = "publickey"; };
+            };
+            github = {
+              host = "github.com";
+              identityFile = config.sops.secrets.GH_SSH_KEY.path;
+              extraOptions = { PreferredAuthentications = "publickey"; };
+            };
           };
-          svr = {
-            host = "SVR";
-            hostname = "10.10.2.1";
-            user = "svr";
-            port = 6523;
-            identityFile = config.sops.secrets.SVR_SSH_KEY.path;
-          };
-          space-x = {
-            host = "DS";
-            hostname = "10.10.2.1";
-            user = "x";
-            port = 10001;
-            identityFile = config.sops.secrets.DS_SSH_KEY.path;
-          };
-          gitlab = {
-            host = "gitlab.com";
-            identityFile = config.sops.secrets.GL_SSH_KEY.path;
-            extraOptions = { PreferredAuthentications = "publickey"; };
-          };
-          github = {
-            host = "github.com";
-            identityFile = config.sops.secrets.GH_SSH_KEY.path;
-            extraOptions = { PreferredAuthentications = "publickey"; };
-          };
+          userKnownHostsFile = [
+            "${homeDirectory}/.ssh/known_hosts"
+
+            # This to avoid fingerprints verification prompt on git clone
+            (pkgs.writeText "gitlab"
+              "gitlab.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf")
+            (pkgs.writeText "github"
+              "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl")
+          ];
         };
       };
-    };
   };
 }
