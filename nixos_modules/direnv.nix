e6e0b55f5ba3b1df7ac93940bedc95cf402b6d66
CLI Arena <git@cliarena.com>
2025-03-25 15:03:49 +0000

    feat(direnv): auto allow dotfiles & project folders

--- nixos_modules/direnv.nix
+++ nixos_modules/direnv.nix
@@ -3,6 +3,7 @@ let
   module = "_direnv";
   description = "auto load environments";
   inherit (lib) mkEnableOption mkIf;
+  inherit (config.home) homeDirectory;
 in {
 
   options.${module}.enable = mkEnableOption description;
@@ -14,6 +15,13 @@ in {
         direnv = {
           enable = true;
           nix-direnv.enable = true;
+          config = {
+            whitelist.prefix = [
+              "${homeDirectory}/dotfiles"
+              "${homeDirectory}/project_main"
+              "${homeDirectory}/project_seconday"
+            ];
+          };
         };
       };
     };
