f8d9811f96a85123ae9105d5f8de5383c2e2d3ff
CLI Arena <git@cliarena.com>
2025-03-25 15:06:05 +0000

    fix(wezterm): disabled tab bar

--- nixos_modules/home_wezterm.nix
+++ nixos_modules/home_wezterm.nix
@@ -76,6 +76,7 @@ in {
                 bottom = 0,
               },
 
+              enable_tab_bar = false,
               tab_bar_at_bottom = true,
               use_fancy_tab_bar = false,
               hide_tab_bar_if_only_one_tab = true,
