3f31eed2079285f4c243e65655241dc6e507dfb1
CLI Arena <git@cliarena.com>
2025-03-25 15:29:43 +0000

    fix(direnv): home config

--- nixos_modules/direnv.nix
+++ nixos_modules/direnv.nix
@@ -3,27 +3,28 @@ let
   module = "_direnv";
   description = "auto load environments";
   inherit (lib) mkEnableOption mkIf;
-  inherit (config.home) homeDirectory;
 in {
 
   options.${module}.enable = mkEnableOption description;
 
   config = mkIf config.${module}.enable {
 
-    home-manager.users.${host.user} = {
-      programs = {
-        direnv = {
-          enable = true;
-          nix-direnv.enable = true;
-          config = {
-            whitelist.prefix = [
-              "${homeDirectory}/dotfiles"
-              "${homeDirectory}/project_main"
-              "${homeDirectory}/project_seconday"
-            ];
+    home-manager.users.${host.user} = { config, ... }:
+      let inherit (config.home) homeDirectory;
+      in {
+        programs = {
+          direnv = {
+            enable = true;
+            nix-direnv.enable = true;
+            config = {
+              whitelist.prefix = [
+                "${homeDirectory}/dotfiles"
+                "${homeDirectory}/project_main"
+                "${homeDirectory}/project_seconday"
+              ];
+            };
           };
         };
       };
-    };
   };
 }
