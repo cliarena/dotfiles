67a2ed3166644261bd2c1020d909499d4ff85d9f
CLI Arena <git@cliarena.com>
2025-03-26 18:38:30 +0000

    fix(flake): home_notes didn't copy .git folder

--- flake.nix
+++ flake.nix
@@ -5,8 +5,15 @@
     # Cloned on boot
     # Update project urls when needed
     # home_notes.url = "gitlab:persona_code/notes";
-    home_notes.url = "git+ssh://git@gitlab.com/persona_code/notes";
-    home_notes.flake = false;
+    # home_notes.url = "git+ssh://git@gitlab.com/persona_code/notes";
+    # home_notes.flake = false;
+    # home_notes.submodules = true;
+    home_notes = {
+      type = "git";
+      url = "git+ssh://git@gitlab.com/persona_code/notes";
+      flake = false;
+      submodules = true;
+    };
 
     # home_dotfiles.url = "gitlab:cliarena_dotfiles/nixos";
     # home_project_main.url = "gitlab:mallx/products";
