c2d6807f8644a57aeb06f0109c3dd02da64c78f0
CLI Arena <git@cliarena.com>
2025-03-29 16:10:12 +0000

    fix(comin): change flake.nix path

--- nixos_modules/comin.nix
+++ nixos_modules/comin.nix
@@ -15,6 +15,7 @@ in {
       remotes = [{
         name = "origin";
         url = "https://gitlab.com/cliarena_dotfiles/nixos";
+        flakeSubdirectory = "./flake";
         # This is an access token to access our private repository
         # auth.access_token_path = cfg.sops.secrets."gitlab/access_token".path;
         # No testing branch on this remote
