8c200f58ebbb7c49b613f3f578e4dc2e533ad704
CLI Arena <git@cliarena.com>
2025-03-29 16:06:00 +0000

    feat(direnv): added stdlib function to persist cache between reboots

--- nixos_modules/direnv.nix
+++ nixos_modules/direnv.nix
@@ -23,6 +23,18 @@ in {
                 "${homeDirectory}/project_seconday"
               ];
             };
+            stdlib = ''
+              : "''${XDG_CACHE_HOME:="/srv/.cache"}"
+              declare -A direnv_layout_dirs
+              direnv_layout_dir() {
+                  local hash path
+                  echo "''${direnv_layout_dirs[$PWD]:=$(
+                      hash="$(sha1sum - <<< "$PWD" | head -c40)"
+                      path="''${PWD//[^a-zA-Z0-9]/-}"
+                      echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+                  )}"
+              };
+            '';
           };
         };
       };
