21f6cde07e56dba1aec0ba9d58c539d89cf5729e
CLI Arena <git@cliarena.com>
2025-05-03 23:37:26 +0100

    fix(tokens_graph): yarn needed hash

--- nixos_modules/tokens_graph.nix
+++ nixos_modules/tokens_graph.nix
@@ -17,13 +17,12 @@ let
       repo = "graph-engine";
       rev = "d651ccc3ceb1e5600c564f7783d534e100a26aa5";
       hash = "sha256-xSJ8ff+np7Nnd21voESMHmyTa3yNYazSaVoRgZ1fXBM=";
-      # hash = lib.fakeHash;
     };
     # inherit version src;
 
     yarnOfflineCache = pkgs.fetchYarnDeps {
       yarnLock = "${src}/yarn.lock";
-      # hash = "sha256-OTGTpAAxr8rmCi5oEWIWzwZqiP3Cx3vyc3r2kbcLyUg=";
+      hash = "sha256-H88dGQolTo70Q5Qvck77TR2GPGnKHl4PICyVwvLn4AQ=";
     };
 
     nativeBuildInputs = with pkgs; [
