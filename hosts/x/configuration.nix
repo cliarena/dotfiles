400a5ce6ee1405ab5f922f4db8098b5146d1d75a
CLIarena <git@cliarena.com>
2025-06-25 18:15:46 +0100

    flake(hosts.x): enabled kmonad

--- hosts/x/configuration.nix
+++ hosts/x/configuration.nix
@@ -15,7 +15,7 @@ in {
   profiles.common.enable = true;
   profiles.desktop.enable = true;
 
-#  _kmonad.enable = true;
+  _kmonad.enable = true;
   _sddm.enable = true;
 
   # _gaming.enable = true;
