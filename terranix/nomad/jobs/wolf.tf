c01a06399de195f55050b29106df6f365f5b6c36
CLIarena <git@cliarena.com>
2025-07-20 20:27:25 +0100

    fix(nomad.wolf): so slow w/ unused cpu

--- terranix/nomad/jobs/wolf.tf
+++ terranix/nomad/jobs/wolf.tf
@@ -61,15 +61,16 @@ job "wolf" {
       driver = "raw_exec"
 
       resources {
-        cpu    = 4096
+       # cpu    = 4096
+        cores = 6
         memory = 4096
         memory_max = 16348
       }
 
       config {
-        command = "/run/current-system/sw/bin/podman"
-        args = [ "import", "http://10.10.0.10:9999/job/nixos/main/x86_64-linux.dev/latest/download/nixos-system-x86_64-linux.tar.xz", "nixos" ]
-#        args = [ "-c", "/run/current-system/sw/bin/wget http://10.10.0.10:9999/job/nixos/main/x86_64-linux.dev/latest/download/nixos-system-x86_64-linux.tar.xz; /run/current-system/sw/bin/unxz  nixos-system-x86_64-linux.tar.xz; /run/current-system/sw/bin/sudo /run/current-system/sw/bin/podman import nixos-system-x86_64-linux.tar nixos" ]
+        command = "/run/current-system/sw/bin/nu"
+#        args = [ "import", "http://10.10.0.10:9999/job/nixos/main/x86_64-linux.dev/latest/download/nixos-system-x86_64-linux.tar.xz", "nixos" ]
+        args = [ "-c", "/run/current-system/sw/bin/wget http://10.10.0.10:9999/job/nixos/main/x86_64-linux.dev/latest/download/nixos-system-x86_64-linux.tar.xz; /run/current-system/sw/bin/unxz  nixos-system-x86_64-linux.tar.xz; /run/current-system/sw/bin/sudo /run/current-system/sw/bin/podman import nixos-system-x86_64-linux.tar nixos" ]
       }
 
     }
