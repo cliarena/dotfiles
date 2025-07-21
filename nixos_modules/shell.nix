398d6aba93cd16e3ba06ea2f9215b78c6fac8aa3
CLIarena <git@cliarena.com>
2025-07-21 19:59:45 +0100

    fix(modules.shell): removed nushell PATH env customisation

--- nixos_modules/shell.nix
+++ nixos_modules/shell.nix
@@ -87,30 +87,30 @@ in {
           configFile.text =
             # nu
             ''
-              let carapace_completer = {|spans|
-              carapace $spans.0 nushell ...$spans | from json
-              }
-              $env.config = {
-               show_banner: false,
-               completions: {
-               case_sensitive: false # case-sensitive completions
-               quick: true    # set to false to prevent auto-selecting completions
-               partial: true    # set to false to prevent partial filling of the prompt
-               algorithm: "fuzzy"    # prefix or fuzzy
-               external: {
-               # set to false to prevent nushell looking into $env.PATH to find more suggestions
-                   enable: true
-               # set to lower can improve completion performance at the cost of omitting some options
-                   max_results: 100
-                   completer: $carapace_completer # check 'carapace_completer'
+                let carapace_completer = {|spans|
+                carapace $spans.0 nushell ...$spans | from json
+                }
+                $env.config = {
+                 show_banner: false,
+                 completions: {
+                 case_sensitive: false # case-sensitive completions
+                 quick: true    # set to false to prevent auto-selecting completions
+                 partial: true    # set to false to prevent partial filling of the prompt
+                 algorithm: "fuzzy"    # prefix or fuzzy
+                 external: {
+                 # set to false to prevent nushell looking into $env.PATH to find more suggestions
+                     enable: true
+                 # set to lower can improve completion performance at the cost of omitting some options
+                     max_results: 100
+                     completer: $carapace_completer # check 'carapace_completer'
+                   }
                  }
-               }
-              }
-              $env.PATH = ($env.PATH |
-              split row (char esep) |
-              prepend /home/myuser/.apps |
-              append /usr/bin/env
-              )'';
+                }
+              #  $env.PATH = ($env.PATH |
+              #  split row (char esep) |
+              #  prepend /home/myuser/.apps |
+              #  append /usr/bin/env)
+            '';
           # OLD config
           # configFile.text = # nu
           #   ''
