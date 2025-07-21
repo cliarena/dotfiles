8009af4e35696a30f9e4db98fed72095b4914e01
CLIarena <git@cliarena.com>
2025-07-21 12:43:57 +0100

    fix(moduls.hydra-image-importer): changed target image

--- nixos_modules/hydra_image_importer.nix
+++ nixos_modules/hydra_image_importer.nix
@@ -9,8 +9,8 @@ let
   module = "_hydra_image_importer";
   description = "imports images from hydra";
   inherit (lib) mkEnableOption mkIf;
-  wolf_bin = "${inputs.wolf.packages.x86_64-linux.default}/bin/wolf";
 
+  image_name = "main";
 in
 {
 
@@ -31,7 +31,7 @@ systemd.timers."hydra-image-importer" = {
 systemd.services."hydra-image-importer" = {
   script = ''
     set -eu
-     ${pkgs.wget}/bin/wget http://10.10.0.10:9999/job/nixos/main/x86_64-linux.dev/latest/download/nixos-system-x86_64-linux.tar.xz && ${pkgs.xz}/bin/unxz nixos-system-x86_64-linux.tar.xz && ${pkgs.podman}/bin/podman import nixos-system-x86_64-linux.tar nixos; ${pkgs.coreutils}/bin/rm -f nixos-system-x86_64-linux.tar nixos-system-x86_64-linux.tar.xz
+     ${pkgs.wget}/bin/wget http://10.10.0.10:9999/job/nixos/main/x86_64-linux.${image_name}/latest/download/nixos-system-x86_64-linux.tar.xz && ${pkgs.xz}/bin/unxz nixos-system-x86_64-linux.tar.xz && ${pkgs.podman}/bin/podman import nixos-system-x86_64-linux.tar nixos; ${pkgs.coreutils}/bin/rm -f nixos-system-x86_64-linux.tar nixos-system-x86_64-linux.tar.xz
   '';
   serviceConfig = {
     Type = "oneshot";
