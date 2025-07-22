ac8ba709fdd148a791426d7a1ef6cf3a1ddb45bb
CLIarena <git@cliarena.com>
2025-07-22 16:34:33 +0100

    fix(modules.shell): missing pkgs arg

--- nixos_modules/shell.nix
+++ nixos_modules/shell.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   inputs,
   host,
   ...
@@ -15,9 +16,8 @@ in {
   options.${module}.enable = mkEnableOption description;
 
   config = mkIf config.${module}.enable {
-
     environment.systemPackages = with pkgs; [atuin];
-   
+
     programs.zoxide.enable = true;
     services.atuin.enable = true;
 
@@ -90,29 +90,29 @@ in {
           configFile.text =
             # nu
             ''
-                let carapace_completer = {|spans|
-                carapace $spans.0 nushell ...$spans | from json
+               let carapace_completer = {|spans|
+               carapace $spans.0 nushell ...$spans | from json
+               }
+               $env.config = {
+                show_banner: false,
+                completions: {
+                case_sensitive: false # case-sensitive completions
+                quick: true    # set to false to prevent auto-selecting completions
+                partial: true    # set to false to prevent partial filling of the prompt
+                algorithm: "fuzzy"    # prefix or fuzzy
+                external: {
+                # set to false to prevent nushell looking into $env.PATH to find more suggestions
+                    enable: true
+                # set to lower can improve completion performance at the cost of omitting some options
+                    max_results: 100
+                    completer: $carapace_completer # check 'carapace_completer'
+                  }
                 }
-                $env.config = {
-                 show_banner: false,
-                 completions: {
-                 case_sensitive: false # case-sensitive completions
-                 quick: true    # set to false to prevent auto-selecting completions
-                 partial: true    # set to false to prevent partial filling of the prompt
-                 algorithm: "fuzzy"    # prefix or fuzzy
-                 external: {
-                 # set to false to prevent nushell looking into $env.PATH to find more suggestions
-                     enable: true
-                 # set to lower can improve completion performance at the cost of omitting some options
-                     max_results: 100
-                     completer: $carapace_completer # check 'carapace_completer'
-                   }
-                 }
-                }
-                $env.PATH = ($env.PATH |
-               split row (char esep) |
-                prepend /home/myuser/.apps |
-                append /usr/bin/env)
+               }
+               $env.PATH = ($env.PATH |
+              split row (char esep) |
+               prepend /home/myuser/.apps |
+               append /usr/bin/env)
             '';
           # OLD config
           # configFile.text = # nu
