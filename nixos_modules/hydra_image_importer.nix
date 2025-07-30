5d5abd7c095b70395adc3eee49475410b6ac36e7
CLIarena <git@cliarena.com>
2025-07-30 02:10:09 +0100

    fix(modules.hydra_image_importer): ram overusage

--- nixos_modules/hydra_image_importer.nix
+++ nixos_modules/hydra_image_importer.nix
@@ -10,6 +10,7 @@
   inherit (lib) mkEnableOption mkIf;
 
   image_name = "main";
+  image_url = "http://10.10.0.10:9999/job/nixos/main/x86_64-linux.${image_name}/latest/download/nixos-system-x86_64-linux.tar.xz";
 in {
   options.${module}.enable = mkEnableOption description;
 
@@ -25,14 +26,14 @@ in {
 
     systemd.services."hydra-image-importer" = {
       script = ''
-       ${pkgs.podman}/bin/podman rmi -f nixos
-        && ${pkgs.wget}/bin/wget http://10.10.0.10:9999/job/nixos/main/x86_64-linux.${image_name}/latest/download/nixos-system-x86_64-linux.tar.xz
-        && ${pkgs.xz}/bin/unxz nixos-system-x86_64-linux.tar.xz
-        && ${pkgs.coreutils}/bin/rm nixos-system-x86_64-linux.tar.xz
-        && ${pkgs.podman}/bin/podman import nixos-system-x86_64-linux.tar nixos
-        && ${pkgs.coreutils}/bin/rm nixos-system-x86_64-linux.tar
-     '';
-     # postStop = "${pkgs.coreutils}/bin/rm -f nixos-system-x86_64-linux.tar nixos-system-x86_64-linux.tar.xz";
+        ${pkgs.podman}/bin/podman rmi -f nixos                                     \
+         && ${pkgs.wget}/bin/wget ${image_url}                                     \
+         && ${pkgs.xz}/bin/unxz nixos-system-x86_64-linux.tar.xz                   \
+         && ${pkgs.coreutils}/bin/rm nixos-system-x86_64-linux.tar.xz              \
+         && ${pkgs.podman}/bin/podman import nixos-system-x86_64-linux.tar nixos   \
+         && ${pkgs.coreutils}/bin/rm nixos-system-x86_64-linux.tar
+      '';
+      # postStop = "${pkgs.coreutils}/bin/rm -f nixos-system-x86_64-linux.tar nixos-system-x86_64-linux.tar.xz";
 
       serviceConfig = {
         Type = "oneshot";
