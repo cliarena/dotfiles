fa9bedde1498a0f6b0331b059561c4f8f10fe120
CLIarena <git@cliarena.com>
2025-09-21 19:01:42 +0100

    fix(river): outdated config

--- nixos_modules/river.nix
+++ nixos_modules/river.nix
@@ -96,7 +96,7 @@ in
             #  terminal
             "'${pkgs.coreutils}/bin/rm -f /home/${host.user}/.config/BraveSoftware/Brave-Browser/Singleton* /home/${host.user}/.config/chromium/Singleton*'" # fixs brave profile appears to be in use by another process. due to changing of hostname inside docker containers
             "'${pkgs.wlr-randr}/bin/wlr-randr --output WL-1 --custom-mode ${resolution} --scale 1.3'" # change scale for zoom
-            "'${pkgs.river}/bin/rivertile -view-padding 1 -outer-padding 3'"
+            "'${pkgs.river-classic}/bin/rivertile -view-padding 1 -outer-padding 3'"
             "'${pkgs.coreutils}/bin/shuf -zen1 /srv/library/wallpapers/* | ${pkgs.findutils}/bin/xargs -0 ${pkgs.wbg}/bin/wbg'"
             #  "'${pkgs.coreutils}/bin/shuf -zen1 /srv/wallpapers/* | ${pkgs.findutils}/bin/xargs -0 ${pkgs.swww}/bin/swww img'"
             #  "${pkgs.swww}/bin/swww-daemon"
