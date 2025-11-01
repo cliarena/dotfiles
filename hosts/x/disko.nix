977cb8ea40fe03a752a9a4e47ea942cf4abba78f
CLI Arena <git@cliarena.com>
2025-11-01 12:19:42 +0100

    ..

--- hosts/x/disko.nix
+++ hosts/x/disko.nix
@@ -36,7 +36,8 @@ in {
                 format = "vfat";
                 mountpoint = "/firmware";
                 # postMountHook =pkgs: toString (pkgs.writeScript "postMountHook.sh" ''
-                postMountHook =pkgs:  pkgs.writeScript "postMountHook.sh" ''
+                # postMountHook =pkgs:  pkgs.writeScript "postMountHook.sh" ''
+                postMountHook =pkgs: ''
                   cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf *.dtb /mnt/firmware
                   cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin /mnt/firmware/u-boot-rpi4.bin
                   cp ${config_txt} /mnt/firmware/config.txt
