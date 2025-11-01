9e44eb9a4e81c1e230afb991312b6949ba7d6c2a
CLI Arena <git@cliarena.com>
2025-11-01 14:49:26 +0100

    ..

--- hosts/x/disko.nix
+++ hosts/x/disko.nix
@@ -1,75 +1,31 @@
-{
-  # pkgs,
-  # disks ? ["/dev/sda"],
-  ...
-}: let
-   disk = "/dev/sda";
-  config_txt =pkgs: pkgs.writeText "config.txt" ''
-    [pi4]
-    kernel=u-boot-rpi4.bin
-    enable_gic=1
-    disable_overscan=1
-
-    [all]
-    arm_64bit=1
-    enable_uart=1
-    avoid_warnings=1
-    dtoverlay=vc4-kms-v3d
-  '';
-  boot_folder =pkgs: /. +  "${pkgs.raspberrypifw}/share/raspberrypi/boot";
-in {
+{disks ? ["/dev/sda"], ...}: {
   disko.devices = {
     disk = {
       main = {
         type = "disk";
-        device =  disk;
-        # device = builtins.elemAt disks 0;
+        device = builtins.elemAt disks 0;
         content = {
           type = "gpt";
           partitions = {
-            firmware = {
-              # start = "2M";
-              size = "30M";
-              priority = 1;
-              type = "0700";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/firmware";
-                # postMountHook =pkgs: toString (pkgs.writeScript "postMountHook.sh" ''
-                # postMountHook =pkgs:  pkgs.writeScript "postMountHook.sh" ''
-                #   (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf *.dtb /mnt/firmware)
-                #   cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin /mnt/firmware/u-boot-rpi4.bin
-                #   cp ${config_txt} /mnt/firmware/config.txt
-                # '';
-                postMountHook = ''
-                   cp ${/. + config_txt} /mnt/firmware/config.txt
-                 '' ;
-                #  extraArgs = [ "-n boot" ];
-                #  device = "/dev/sda1";
-              };
-            };
-            boot = {
-              # start = "2M";
-              size = "1G";
-              # priority =1;
+            ESP = {
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
+                # label = "ESP";
                 mountpoint = "/boot";
-                #  extraArgs = [ "-n boot" ];
-                #  device = "/dev/sda1";
+                #  extraArgs = [ "-n ESP" ];
+                #  device = "/dev/disk/by-partlabel/ESP";
               };
             };
             nixos = {
-              # start = "512M";
-              #  end = "100%";
               size = "100%";
               content = {
                 type = "btrfs";
-                #    extraArgs = [ "--label nixos" ]; # Override existing partition
-                #    device = "/dev/sda2";
+                # label = "NIXOS";
+                # extraArgs = [ "-n NIXOS" ]; # Override existing partition
+                # device = "/dev/disk/by-partlabel/NIXOS";
                 subvolumes = {
                   # Mountpoints inferred from subvolume name
                   "/nix" = {
---
+++ hosts/x/disko_no_uefi.nix
@@ -0,0 +1,108 @@
+{
+  # pkgs,
+  # disks ? ["/dev/sda"],
+  ...
+}: let
+   disk = "/dev/sda";
+  config_txt =pkgs: pkgs.writeText "config.txt" ''
+    [pi4]
+    kernel=u-boot-rpi4.bin
+    enable_gic=1
+    disable_overscan=1
+
+    [all]
+    arm_64bit=1
+    enable_uart=1
+    avoid_warnings=1
+    dtoverlay=vc4-kms-v3d
+  '';
+  boot_folder =pkgs: /. +  "${pkgs.raspberrypifw}/share/raspberrypi/boot";
+in {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device =  disk;
+        # device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            firmware = {
+              # start = "2M";
+              size = "30M";
+              priority = 1;
+              type = "0700";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/firmware";
+                # postMountHook =pkgs: toString (pkgs.writeScript "postMountHook.sh" ''
+                # postMountHook =pkgs:  pkgs.writeScript "postMountHook.sh" ''
+                #   (cd ${pkgs.raspberrypifw}/share/raspberrypi/boot && cp bootcode.bin fixup*.dat start*.elf *.dtb /mnt/firmware)
+                #   cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin /mnt/firmware/u-boot-rpi4.bin
+                #   cp ${config_txt} /mnt/firmware/config.txt
+                # '';
+                postMountHook = ''
+                   cp ${/. + config_txt} /mnt/firmware/config.txt
+                 '' ;
+                #  extraArgs = [ "-n boot" ];
+                #  device = "/dev/sda1";
+              };
+            };
+            boot = {
+              # start = "2M";
+              size = "1G";
+              # priority =1;
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                #  extraArgs = [ "-n boot" ];
+                #  device = "/dev/sda1";
+              };
+            };
+            nixos = {
+              # start = "512M";
+              #  end = "100%";
+              size = "100%";
+              content = {
+                type = "btrfs";
+                #    extraArgs = [ "--label nixos" ]; # Override existing partition
+                #    device = "/dev/sda2";
+                subvolumes = {
+                  # Mountpoints inferred from subvolume name
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "/log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = ["compress=zstd" "noatime" "noexec"];
+                  };
+                  "/srv" = {
+                    mountpoint = "/srv";
+                    mountOptions = ["compress=zstd" "noatime"];
+                  };
+                  "/swap" = {
+                    mountpoint = "/swap";
+                    mountOptions = ["compress=zstd" "noatime" "noexec"];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "defaults"
+        "size=6G" # limit tmpfs size to 6GiB
+        "noatime"
+        "mode=755"
+      ];
+    };
+  };
+}
