ec8ca28926126ab73ab8f804aa834e7ce83dedd7
CLI Arena <git@cliarena.com>
2025-11-01 13:19:09 +0100

    ..

--- hosts/x/disko.nix
+++ hosts/x/disko.nix
@@ -42,7 +42,9 @@ in {
                 #   cp ${pkgs.ubootRaspberryPi4_64bit}/u-boot.bin /mnt/firmware/u-boot-rpi4.bin
                 #   cp ${config_txt} /mnt/firmware/config.txt
                 # '';
-                postMountHook = toString boot_folder;
+                postMountHook = ''
+                   cp ${config_txt} /mnt/firmware/config.txt
+                 '' ;
                 #  extraArgs = [ "-n boot" ];
                 #  device = "/dev/sda1";
               };
