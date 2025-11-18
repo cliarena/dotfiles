d3cea3c64e1525253378b787bb09b98e8a1807ea
CLI Arena <git@cliarena.com>
2025-11-18 19:06:55 +0100

    fix(hydra_image_importer): moonlight can't start nixos at boot

--- nixos_modules/hydra_image_importer.nix
+++ nixos_modules/hydra_image_importer.nix
@@ -28,6 +28,7 @@ in {
     systemd.services."hydra-image-importer" = {
         # ${pkgs.podman}/bin/podman rmi -f nixos  # no longer needed to free memory image is saved in disk
       script = ''
+        ${pkgs.podman}/bin/podman rmi -f nixos                                  \ # removes dead nixos containers which doesn't let moonlight start it
         ${pkgs.wget}/bin/wget ${image_url}                                      \
         && ${pkgs.xz}/bin/unxz nixos-system-x86_64-linux.tar.xz                 \
         && ${pkgs.podman}/bin/podman import nixos-system-x86_64-linux.tar nixos \
