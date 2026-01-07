860eca608589fec105649ceaa505c1d28bdda870
CLI Arena <git@cliarena.com>
2026-01-07 15:52:33 +0100

    ..

--- nixos_modules/catppuccin.nix
+++ nixos_modules/catppuccin.nix
@@ -2,13 +2,14 @@
   config,
   inputs,
   lib,
+ host,
   ...
 }: let
   module = "_catppuccin";
   description = "catppuccin theme";
   inherit (lib) mkEnableOption mkIf;
 in {
-  imports = [inputs.catppuccin.nixosModules.catppuccin];
+  imports = [inputs.catppuccin.nixosModules.catppuccin inputs.catppuccin.homeModules.catppuccin];
 
   options.${module}.enable = mkEnableOption description;
 
@@ -19,8 +20,13 @@ in {
 
       # Don't use modules with IFD by default
       tty.enable = false;
+    };
+    home-manager.users.${host.user} = {
+    catppuccin = {
+      flavor = "mocha";
       chromium.enable = true;
       qutebrowser.enable = true;
+    };
     };
   };
 }
