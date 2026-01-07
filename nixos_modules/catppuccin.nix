27fde0104435662f1aeb9a2de0c76c3db9bcd1eb
CLI Arena <git@cliarena.com>
2026-01-07 16:51:55 +0100

    ..

--- nixos_modules/catppuccin.nix
+++ nixos_modules/catppuccin.nix
@@ -11,20 +11,20 @@
   inherit (inputs) home-manager catppuccin;
 in {
   imports = [
-    catppuccin.nixosModules.catppuccin
+    # catppuccin.nixosModules.catppuccin
     home-manager.nixosModules.home-manager
   ];
 
   options.${module}.enable = mkEnableOption description;
 
   config = mkIf config.${module}.enable {
-    catppuccin = {
-      enable = true;
-      flavor = "mocha";
-
-      # Don't use modules with IFD by default
-      tty.enable = false;
-    };
+    # catppuccin = {
+    #   enable = true;
+    #   flavor = "mocha";
+    #
+    #   # Don't use modules with IFD by default
+    #   tty.enable = false;
+    # };
     home-manager.users.${host.user} = {
       imports = [catppuccin.homeModules.catppuccin];
      catppuccin = {
