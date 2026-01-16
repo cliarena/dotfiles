d409ce0cf68799198890c2fe5e651870dab09e2d
CLI Arena <git@cliarena.com>
2026-01-16 11:21:52 +0100

    fix(direnv): removed broken caching

--- nixos_modules/direnv.nix
+++ nixos_modules/direnv.nix
@@ -3,43 +3,48 @@
   lib,
   host,
   ...
-}: let
+}:
+let
   module = "_direnv";
   description = "auto load environments";
   inherit (lib) mkEnableOption mkIf;
-in {
+in
+{
   options.${module}.enable = mkEnableOption description;
 
   config = mkIf config.${module}.enable {
-    home-manager.users.${host.user} = {config, ...}: let
-      inherit (config.home) homeDirectory;
-    in {
-      programs = {
-        direnv = {
-          enable = true;
-          nix-direnv.enable = true;
-          config = {
-            hide_env_diff = true;
-            whitelist.prefix = [
-              "${homeDirectory}/dotfiles"
-              "${homeDirectory}/project_main"
-              "${homeDirectory}/project_seconday"
-            ];
-          };
-          stdlib = ''
-            : "''${XDG_CACHE_HOME:="/srv/.cache"}"
-            declare -A direnv_layout_dirs
-            direnv_layout_dir() {
-                local hash path
-                echo "''${direnv_layout_dirs[$PWD]:=$(
-                    hash="$(sha1sum - <<< "$PWD" | head -c40)"
-                    path="''${PWD//[^a-zA-Z0-9]/-}"
-                    echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
-                )}"
+    home-manager.users.${host.user} =
+      { config, ... }:
+      let
+        inherit (config.home) homeDirectory;
+      in
+      {
+        programs = {
+          direnv = {
+            enable = true;
+            nix-direnv.enable = true;
+            config = {
+              hide_env_diff = true;
+              whitelist.prefix = [
+                "${homeDirectory}/dotfiles"
+                "${homeDirectory}/project_main"
+                "${homeDirectory}/project_seconday"
+              ];
             };
-          '';
+            # stdlib = ''
+            #   : "''${XDG_CACHE_HOME:="/srv/.cache"}"
+            #   declare -A direnv_layout_dirs
+            #   direnv_layout_dir() {
+            #       local hash path
+            #       echo "''${direnv_layout_dirs[$PWD]:=$(
+            #           hash="$(sha1sum - <<< "$PWD" | head -c40)"
+            #           path="''${PWD//[^a-zA-Z0-9]/-}"
+            #           echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+            #       )}"
+            #   };
+            # '';
+          };
         };
       };
-    };
   };
 }
