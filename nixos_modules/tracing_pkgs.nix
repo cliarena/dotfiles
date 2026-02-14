f94b3c634732bb7b7b0b58967491aeb7635d60b9
CLI Arena <git@cliarena.com>
2026-02-14 13:41:50 +0100

    fix(config): updated

--- nixos_modules/tracing_pkgs.nix
+++ nixos_modules/tracing_pkgs.nix
@@ -3,11 +3,13 @@
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   module = "_tracing_pkgs";
   description = "tracing_pkgs";
   inherit (lib) mkEnableOption mkIf;
-in {
+in
+{
   options.${module}.enable = mkEnableOption description;
 
   config = mkIf config.${module}.enable {
@@ -19,13 +21,13 @@ in {
     boot.kernel.sysctl."kernel.kptr_restrict" = lib.mkForce 0;
 
     # so perf can find kernel modules
-    systemd.tmpfiles.rules = ["L /lib - - - - /run/current/system/lib"];
+    systemd.tmpfiles.rules = [ "L /lib - - - - /run/current/system/lib" ];
 
     environment.systemPackages = [
       pkgs.strace
 
       # we want to use trace from bcc
-      (pkgs.lowPrio config.boot.kernelPackages.perf)
+      (lib.lowPrio config.boot.perf)
     ];
   };
 }
