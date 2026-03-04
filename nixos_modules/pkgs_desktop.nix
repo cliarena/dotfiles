ffb7fd970ea1379403503d7eb82611d4c0c125e1
CLI Arena <git@cliarena.com>
2026-03-04 14:15:37 +0000

    feat(pkgs_desktop): added openmvg & openmvs

--- nixos_modules/pkgs_desktop.nix
+++ nixos_modules/pkgs_desktop.nix
@@ -50,9 +50,42 @@ in
       blender # use hip to get gpu acceleration
 
       # Guassian Splatting
-      # openmvg
+      openmvg
       # openmvs
       # colmap
+      (openmvs.overrideAttrs (
+        finalAttrs: prevAttrs: {
+
+          version = "2.4.0";
+
+          src = fetchFromGitHub {
+            owner = "cdcseacave";
+            repo = "openmvs";
+            rev = "v${version}";
+            hash = "sha256-0tL2tqHYBQMGL9k+NqTUxieWuDP3YB6X9DcXYnlGWWg=";
+            fetchSubmodules = true;
+          };
+          buildInputs = prevAttrs.buildInputs ++ [
+            libjxl
+            nanoflann
+          ];
+          nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ python3Packages.python ];
+
+          cmakeFlags = prevAttrs.cmakeFlags ++ [
+            (lib.cmakeFeature "Python3_EXECUTABLE" (lib.getExe python3Packages.python))
+          ];
+
+          # checkPhase = ''
+          #   runHook preCheck
+          #   ${lib.optionalString (stdenv.hostPlatform.isLinux && stdenv.hostPlatform.isAarch64) ''
+          #     export KMP_AFFINITY=disabled
+          #     export OMP_PROC_BIND=false
+          #   ''}
+          #   ctest --output-on-failure
+          #   runHook postCheck
+          # '';
+        }
+      ))
 
       ### Tools ###
       file # shows types of files needed by yazi
