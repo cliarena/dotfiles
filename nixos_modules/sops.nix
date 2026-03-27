a4e3f288323034afbedba58e5157554d415bfcda
CLI Arena <git@cliarena.com>
2026-03-27 22:27:50 +0100

    fix(sops): removed hashicorp secrects

--- nixos_modules/sops.nix
+++ nixos_modules/sops.nix
@@ -4,13 +4,15 @@
   inputs,
   pkgs,
   ...
-}: let
+}:
+let
   module = "_sops_hosting";
   description = "secrets for hosting profile";
   inherit (lib) mkEnableOption mkIf;
   inherit (inputs) sops-nix;
-in {
-  imports = [sops-nix.nixosModules.sops];
+in
+{
+  imports = [ sops-nix.nixosModules.sops ];
 
   options.${module}.enable = mkEnableOption description;
 
@@ -18,44 +20,42 @@ in {
     sops = {
       defaultSopsFile = ../secrets/default.yaml;
       age.keyFile = "/srv/secrets/SOPS_AGE_KEY";
-      age.sshKeyPaths =
-        pkgs.lib.mkForce []; # Must set to empty list for age heyfile to work
-      gnupg.sshKeyPaths =
-        pkgs.lib.mkForce []; # Must set to empty list for age heyfile to work
+      age.sshKeyPaths = pkgs.lib.mkForce [ ]; # Must set to empty list for age heyfile to work
+      gnupg.sshKeyPaths = pkgs.lib.mkForce [ ]; # Must set to empty list for age heyfile to work
 
       secrets = {
-        ACME_VAULT_CERT_CREDENTIALS = {};
-        CLOUDFLARE_API_TOKEN = {};
+        ACME_VAULT_CERT_CREDENTIALS = { };
+        CLOUDFLARE_API_TOKEN = { };
         VICTORIA_METRICS_VAULT_TOKEN = {
           # owner = config.users.groups.vmagent.name;
           # group = config.users.groups.vmagent.name;
         };
 
-        "NOMAD_GOSSIP_ENCRYPTION_KEY.hcl" = {};
-
-        # VAULT_ROOT_TOKEN = { sopsFile = ../../secrets/vault.yaml; };
-
-        CONSUL_HTTP_TOKEN = {sopsFile = ../secrets/consul.yaml;};
-        "CONSUL_GOSSIP_ENCRYPTION_KEY.hcl" = {
-          owner = config.users.groups.consul.name;
-          group = config.users.groups.consul.name;
-          sopsFile = ../secrets/consul.yaml;
-        };
-        "CONSUL_ACL_INITIAL_MANAGEMENT_TOKEN.hcl" = {
-          owner = config.users.groups.consul.name;
-          group = config.users.groups.consul.name;
-          sopsFile = ../secrets/consul.yaml;
-        };
-        CONSUL_VAULT_ROLE_ID = {
-          owner = config.users.groups.consul.name;
-          group = config.users.groups.consul.name;
-          sopsFile = ../secrets/consul.yaml;
-        };
-        CONSUL_VAULT_SECRET_ID = {
-          owner = config.users.groups.consul.name;
-          group = config.users.groups.consul.name;
-          sopsFile = ../secrets/consul.yaml;
-        };
+        # "NOMAD_GOSSIP_ENCRYPTION_KEY.hcl" = {};
+        #
+        # # VAULT_ROOT_TOKEN = { sopsFile = ../../secrets/vault.yaml; };
+        #
+        # CONSUL_HTTP_TOKEN = {sopsFile = ../secrets/consul.yaml;};
+        # "CONSUL_GOSSIP_ENCRYPTION_KEY.hcl" = {
+        #   owner = config.users.groups.consul.name;
+        #   group = config.users.groups.consul.name;
+        #   sopsFile = ../secrets/consul.yaml;
+        # };
+        # "CONSUL_ACL_INITIAL_MANAGEMENT_TOKEN.hcl" = {
+        #   owner = config.users.groups.consul.name;
+        #   group = config.users.groups.consul.name;
+        #   sopsFile = ../secrets/consul.yaml;
+        # };
+        # CONSUL_VAULT_ROLE_ID = {
+        #   owner = config.users.groups.consul.name;
+        #   group = config.users.groups.consul.name;
+        #   sopsFile = ../secrets/consul.yaml;
+        # };
+        # CONSUL_VAULT_SECRET_ID = {
+        #   owner = config.users.groups.consul.name;
+        #   group = config.users.groups.consul.name;
+        #   sopsFile = ../secrets/consul.yaml;
+        # };
         # CONSUL_ACL_DEFAULT_TOKEN = { };
 
         # SURREALDB_USER = { };
