3624141173a0424c201d10a4c313c6b7ed63310a
CLI Arena <git@cliarena.com>
2026-06-06 20:06:38 +0100

    fix(nvim): lsp zls server not supporting latest zig version

--- nixos_atoms/nixvim/lsp.nix
+++ nixos_atoms/nixvim/lsp.nix
@@ -30,11 +30,12 @@ in
         };
         zls = {
           enable = true;
-          package = inputs.zls.packages.x86_64-linux.zls.overrideAttrs
-            (old: {
-              nativeBuildInputs =
-                [ inputs.zig_overlay.packages.x86_64-linux.master ];
-            });
+          package = inputs.zls.packages.x86_64-linux.zls.overrideAttrs (old: {
+            nativeBuildInputs = [
+              # inputs.zig_overlay.packages.x86_64-linux.master
+              inputs.zig_overlay.packages.x86_64-linux.default
+            ];
+          });
         };
       };
     };
