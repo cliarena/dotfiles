c4375a001aea3c76c8d717a11a371019c7846742
CLI Arena <git@cliarena.com>
2026-07-21 10:55:34 +0100

    ..

--- nixos_modules/oci_huly.nix
+++ nixos_modules/oci_huly.nix
@@ -20,7 +20,7 @@ let
   # cr_db_url = "postgres://${cr_username}:${cr_secret}@cockroach:26257/${cr_db}";
   cr_db_url = "postgres://${cr_username}:${cr_secret}@${host_addr}:5432/${cr_db}";
 
-  s3_addr = "http://${host_addr}:${ports.s3_rpc}?accessKey=${garage.default_access_key}&secretKey=${garage.default_secret_key}";
+  s3_addr = "s3|http://${host_addr}:${ports.s3_rpc}?accessKey=${garage.default_access_key}&secretKey=${garage.default_secret_key}";
   ports = {
     front = "7070";
     account = "3000";
