95d22657fb7df77e5a01edd4262c5cbae12c60b4
CLI Arena <git@cliarena.com>
2026-07-21 11:13:42 +0100

    ..

--- nixos_modules/oci_huly.nix
+++ nixos_modules/oci_huly.nix
@@ -275,7 +275,8 @@ in
           SERVER_PORT = "${ports.front}";
           SERVER_SECRET = secret;
           LOVE_ENDPOINT = "http://${host_addr}/_love";
-          ACCOUNTS_URL = "http://${host_addr}/_accounts";
+          # ACCOUNTS_URL = "http://${host_addr}/_accounts";
+          ACCOUNTS_URL = "http://${host_addr}:${ports.account}";
           ACCOUNTS_URL_INTERNAL = "http://${host_addr}:${ports.account}";
           REKONI_URL = "http://${host_addr}:${ports.rekoni}";
           CALENDAR_URL = "http://${host_addr}/_calendar";
